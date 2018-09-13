0266b63d5661ff13b341c4ad85bbe7c00fc2acf1
Ron Evans <ron@hybridgroup.com>
2018-09-13 16:14:42 +0200

    example: use const value for button pin number, with comment to edit for a different platform
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- src/examples/button/button.go
+++ src/examples/button/button.go
@@ -5,11 +5,15 @@ import (
 	"runtime"
 )
 
+// this example assumes that the button is connected to pin 8. Change the value below
+// to use a different pin.
+const buttonPin = 8
+
 func main() {
 	led := machine.GPIO{machine.LED}
 	led.Configure(machine.GPIOConfig{Mode: machine.GPIO_OUTPUT})
 
-	button := machine.GPIO{8}
+	button := machine.GPIO{buttonPin}
 	button.Configure(machine.GPIOConfig{Mode: machine.GPIO_INPUT})
 
 	for {
