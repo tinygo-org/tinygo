c0d1963d63bbe5c7819566435e7a4e97850df18c
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-09-18 20:45:02 +0200

    runtime/nrf: remove some leftover header files

--- src/runtime/nrfx_config.h
+++
@@ -1,4 +0,0 @@
-
-#pragma once
-
-#define NRFX_ASSERT(expression) do { bool res = expression; (void)res; } while (0)
--- src/runtime/runtime_nrf.h
+++
@@ -1,6 +0,0 @@
-
-#pragma once
-
-#include <stdint.h>
-
-void rtc_sleep(uint32_t ticks);
