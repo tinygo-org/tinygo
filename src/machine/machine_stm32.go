4d5b5241ecfe63c44f48b1bda8bc2c52a924db73
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-09-22 17:42:44 +0200

    all: avoid _Msk fields when not necessary

--- src/machine/machine_stm32.go
+++ src/machine/machine_stm32.go
@@ -51,7 +51,7 @@ func (p GPIO) Configure(config GPIOConfig) {
 	// Enable clock.
 	// Do this always, as it isn't known whether the clock has already been
 	// enabled.
-	stm32.RCC.APB2ENR |= stm32.RCC_APB2ENR_IOPCEN_Msk;
+	stm32.RCC.APB2ENR |= stm32.RCC_APB2ENR_IOPCEN
 
 	// Configure the GPIO pin.
 	port := p.getPort()
