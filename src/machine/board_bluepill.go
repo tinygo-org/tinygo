3bba26ce5be30b79dc447ca77c34f5008f4b7945
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-09-22 15:42:22 +0200

    machine/bluepill: create definitions for all pins

--- src/machine/board_bluepill.go
+++ src/machine/board_bluepill.go
@@ -4,5 +4,43 @@ package machine
 
 // https://wiki.stm32duino.com/index.php?title=File:Bluepillpinout.gif
 const (
-	LED = portC + 13
+	PA0  = portA + 0
+	PA1  = portA + 1
+	PA2  = portA + 2
+	PA3  = portA + 3
+	PA4  = portA + 4
+	PA5  = portA + 5
+	PA6  = portA + 6
+	PA7  = portA + 7
+	PA8  = portA + 8
+	PA9  = portA + 9
+	PA10 = portA + 10
+	PA11 = portA + 11
+	PA12 = portA + 12
+	PA13 = portA + 13
+	PA14 = portA + 14
+	PA15 = portA + 15
+	PB0  = portB + 0
+	PB1  = portB + 1
+	PB2  = portB + 2
+	PB3  = portB + 3
+	PB4  = portB + 4
+	PB5  = portB + 5
+	PB6  = portB + 6
+	PB7  = portB + 7
+	PB8  = portB + 8
+	PB9  = portB + 9
+	PB10 = portB + 10
+	PB11 = portB + 11
+	PB12 = portB + 12
+	PB13 = portB + 13
+	PB14 = portB + 14
+	PB15 = portB + 15
+	PC13 = portC + 13
+	PC14 = portC + 14
+	PC15 = portC + 15
+)
+
+const (
+	LED = PC13
 )
