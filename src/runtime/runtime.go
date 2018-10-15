a613d0484e5de282d82170bd11f0a1151e863659
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-10-15 20:20:37 +0200

    runtime: add support for time.Now()
    TODO: On unix systems, this does not return an accurate value.

--- src/runtime/runtime.go
+++ src/runtime/runtime.go
@@ -98,3 +98,11 @@ func sliceCopy(dst, src unsafe.Pointer, dstLen, srcLen lenType, elemSize uintptr
 func sleep(d int64) {
 	sleepTicks(timeUnit(d / tickMicros))
 }
+
+//go:linkname now time.now
+func now() (sec int64, nsec int32, mono int64) {
+	mono = int64(ticks()) * tickMicros
+	sec = mono / (1000 * 1000 * 1000)
+	nsec = int32(mono - sec*(1000*1000*1000))
+	return
+}
