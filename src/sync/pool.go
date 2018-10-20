7f60dd79eecde2dbd86ef6f2562e2360baafd975
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-10-20 15:52:41 +0200

    sync: implement dummy sync.Pool for fmt

---
+++ src/sync/pool.go
@@ -0,0 +1,16 @@
+package sync
+
+// Pool is a very simple implementation of sync.Pool. It does not actually
+// implement a pool.
+type Pool struct {
+	New func() interface{}
+}
+
+// Get returns the value of calling Pool.New().
+func (p *Pool) Get() interface{} {
+	return p.New()
+}
+
+// Put drops the value put into the pool.
+func (p *Pool) Put(x interface{}) {
+}
