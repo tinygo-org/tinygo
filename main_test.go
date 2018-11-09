c3c4a33b4275b6fa28ee79fe6caf2aaaac73c719
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-11-09 16:31:21 +0100

    main: enable -initinterp by default
    It can still be disabled using -initinterp=false.

--- main_test.go
+++ main_test.go
@@ -65,7 +65,7 @@ func runTest(path, tmpdir string, target string, t *testing.T) {
 		dumpSSA:    false,
 		debug:      false,
 		printSizes: "",
-		initInterp: false,
+		initInterp: true,
 	}
 	binary := filepath.Join(tmpdir, "test")
 	err = Build(path, binary, target, config)
