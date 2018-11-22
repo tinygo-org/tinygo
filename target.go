62d74d83290032480da316a9e7f4069678ee6f13
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-11-22 13:24:13 +0100

    all: compile and link using clang, where possible

--- target.go
+++ target.go
@@ -18,18 +18,21 @@ import (
 // https://doc.rust-lang.org/nightly/nightly-rustc/rustc_target/spec/struct.TargetOptions.html
 // https://github.com/shepmaster/rust-arduino-blink-led-no-core-with-cargo/blob/master/blink/arduino.json
 type TargetSpec struct {
-	Inherits    []string `json:"inherits"`
-	Triple      string   `json:"llvm-target"`
-	BuildTags   []string `json:"build-tags"`
-	Linker      string   `json:"linker"`
-	RTLib       string   `json:"rtlib"` // compiler runtime library (libgcc, compiler-rt)
-	PreLinkArgs []string `json:"pre-link-args"`
-	Objcopy     string   `json:"objcopy"`
-	Emulator    []string `json:"emulator"`
-	Flasher     string   `json:"flash"`
-	OCDDaemon   []string `json:"ocd-daemon"`
-	GDB         string   `json:"gdb"`
-	GDBCmds     []string `json:"gdb-initial-cmds"`
+	Inherits   []string `json:"inherits"`
+	Triple     string   `json:"llvm-target"`
+	BuildTags  []string `json:"build-tags"`
+	Compiler   string   `json:"compiler"`
+	Linker     string   `json:"linker"`
+	RTLib      string   `json:"rtlib"` // compiler runtime library (libgcc, compiler-rt)
+	CFlags     []string `json:"cflags"`
+	LDFlags    []string `json:"ldflags"`
+	ExtraFiles []string `json:"extra-files"`
+	Objcopy    string   `json:"objcopy"`
+	Emulator   []string `json:"emulator"`
+	Flasher    string   `json:"flash"`
+	OCDDaemon  []string `json:"ocd-daemon"`
+	GDB        string   `json:"gdb"`
+	GDBCmds    []string `json:"gdb-initial-cmds"`
 }
 
 // copyProperties copies all properties that are set in spec2 into itself.
@@ -41,15 +44,18 @@ func (spec *TargetSpec) copyProperties(spec2 *TargetSpec) {
 		spec.Triple = spec2.Triple
 	}
 	spec.BuildTags = append(spec.BuildTags, spec2.BuildTags...)
+	if spec2.Compiler != "" {
+		spec.Compiler = spec2.Compiler
+	}
 	if spec2.Linker != "" {
 		spec.Linker = spec2.Linker
 	}
 	if spec2.RTLib != "" {
 		spec.RTLib = spec2.RTLib
 	}
-	if len(spec2.PreLinkArgs) != 0 {
-		spec.PreLinkArgs = spec2.PreLinkArgs
-	}
+	spec.CFlags = append(spec.CFlags, spec2.CFlags...)
+	spec.LDFlags = append(spec.LDFlags, spec2.LDFlags...)
+	spec.ExtraFiles = append(spec.ExtraFiles, spec2.ExtraFiles...)
 	if spec2.Objcopy != "" {
 		spec.Objcopy = spec2.Objcopy
 	}
@@ -143,13 +149,13 @@ func LoadTarget(target string) (*TargetSpec, error) {
 		// No target spec available. Use the default one, useful on most systems
 		// with a regular OS.
 		*spec = TargetSpec{
-			Triple:      target,
-			BuildTags:   []string{runtime.GOOS, runtime.GOARCH},
-			Linker:      "cc",
-			PreLinkArgs: []string{"-no-pie"}, // WARNING: clang < 5.0 requires -nopie
-			Objcopy:     "objcopy",
-			GDB:         "gdb",
-			GDBCmds:     []string{"run"},
+			Triple:    target,
+			BuildTags: []string{runtime.GOOS, runtime.GOARCH},
+			Linker:    "cc",
+			LDFlags:   []string{"-no-pie"}, // WARNING: clang < 5.0 requires -nopie
+			Objcopy:   "objcopy",
+			GDB:       "gdb",
+			GDBCmds:   []string{"run"},
 		}
 		return spec, nil
 	}
