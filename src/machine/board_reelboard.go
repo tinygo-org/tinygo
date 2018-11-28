82fb80a94d2acfc18c78a4d395c6d9baa6908677
Ron Evans <ron@hybridgroup.com>
2018-11-28 11:59:48 +0100

    reelboard: add standard LED definitions for built-in LEDs
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- src/machine/board_reelboard.go
+++ src/machine/board_reelboard.go
@@ -6,7 +6,11 @@ const HasLowFrequencyCrystal = true
 
 // LEDs on the reel board
 const (
-	LED        = LED_YELLOW
+	LED        = LED1
+	LED1       = LED_YELLOW
+	LED2       = LED_RED
+	LED3       = LED_GREEN
+	LED4       = LED_BLUE
 	LED_RED    = 11
 	LED_GREEN  = 12
 	LED_BLUE   = 41
