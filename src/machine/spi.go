c8d39e26d1809bca2bb54b42178b7198f7e05309
Ron Evans <ron@hybridgroup.com>
2018-12-01 21:29:58 +0100

    machine/nrf: improve godocs formatting of Tx method
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- src/machine/spi.go
+++ src/machine/spi.go
@@ -12,14 +12,21 @@ var (
 // Tx handles read/write operation for SPI interface. Since SPI is a syncronous write/read
 // interface, there must always be the same number of bytes written as bytes read.
 // The Tx method knows about this, and offers a few different ways of calling it.
+//
 // This form sends the bytes in tx buffer, putting the resulting bytes read into the rx buffer.
 // Note that the tx and rx buffers must be the same size:
-// spi.Tx(tx, rx)
+//
+// 		spi.Tx(tx, rx)
+//
 // This form sends the tx buffer, ignoring the result. Useful for sending "commands" that return zeros
 // until all the bytes in the command packet have been received:
-// spi.Tx(tx, nil)
+//
+// 		spi.Tx(tx, nil)
+//
 // This form sends zeros, putting the result into the rx buffer. Good for reading a "result packet":
-// spi.Tx(nil, rx)
+//
+// 		spi.Tx(nil, rx)
+//
 func (spi SPI) Tx(w, r []byte) error {
 	if w == nil && r == nil {
 		return ErrTxSlicesRequired
