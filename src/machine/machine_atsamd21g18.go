5254e0e2be9ac7d8f79ace0b632cd780ce4b323b
Ron Evans <ron@hybridgroup.com>
2019-01-17 23:44:06 +0100

    machine/atsamd21g18: adds GPIO Get() function
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- src/machine/machine_atsamd21g18.go
+++ src/machine/machine_atsamd21g18.go
@@ -19,7 +19,16 @@ const (
 
 // Configure this pin with the given configuration.
 func (p GPIO) Configure(config GPIOConfig) {
-	sam.PORT.DIRSET0 = (1 << p.Pin)
+	if config.Mode == GPIO_OUTPUT { // set output bit
+		sam.PORT.DIRSET0 = (1 << p.Pin)
+	} else {
+		sam.PORT.DIRCLR0 = (1 << p.Pin)
+	}
+}
+
+// Get returns the current value of a GPIO pin.
+func (p GPIO) Get() bool {
+	return (sam.PORT.IN0>>p.Pin)&1 > 0
 }
 
 // Set the pin to high or low.
