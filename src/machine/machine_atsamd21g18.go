baa34cc7ce69326d5f1c67d503a2f9c7a184dbc7
Ron Evans <ron@hybridgroup.com>
2019-01-17 23:26:56 +0100

    machine/atsamd21g18: remove constants not in use
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- src/machine/machine_atsamd21g18.go
+++ src/machine/machine_atsamd21g18.go
@@ -8,12 +8,6 @@ import (
 
 const CPU_FREQUENCY = 48000000
 
-const (
-	SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ = 32768
-	SOC_ATMEL_SAM0_OSC8M_FREQ_HZ   = 8000000
-	SOC_ATMEL_SAM0_GCLK1_FREQ_HZ   = SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ
-)
-
 // Peripheral abstraction layer for the atsamd21g18.
 
 type GPIOMode uint8
