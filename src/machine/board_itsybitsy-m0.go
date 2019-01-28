f685c9e38dd5404675bd785be025db70f7f4d798
Ron Evans <ron@hybridgroup.com>
2019-01-28 12:40:06 +0100

    machine/atsamd21: implement PWM interface for all pins that support it
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- src/machine/board_itsybitsy-m0.go
+++ src/machine/board_itsybitsy-m0.go
@@ -4,20 +4,20 @@ package machine
 
 // GPIO Pins
 const (
-	D0  = 11 // UART0 RX: SERCOM0/PAD[3]
-	D1  = 10 // UART0 TX: SERCOM0/PAD[2]
+	D0  = 11 // UART0 RX
+	D1  = 10 // UART0 TX
 	D2  = 14
-	D3  = 9
-	D4  = 8
-	D5  = 15
-	D6  = 20
+	D3  = 9  // PWM available
+	D4  = 8  // PWM available
+	D5  = 15 // PWM available
+	D6  = 20 // PWM available
 	D7  = 21
-	D8  = 6
-	D9  = 7
-	D10 = 18 // UART1 TX(1): SERCOM1/PAD[2] can be used for UART1 TX
-	D11 = 16 // UART1 TX(2): SERCOM1/PAD[0] can be used for UART1 TX
-	D12 = 19
-	D13 = 17
+	D8  = 6  // PWM available
+	D9  = 7  // PWM available
+	D10 = 18 // can be used for PWM or UART1 RX
+	D11 = 16 // can be used for PWM or UART1 TX
+	D12 = 19 // PWM available
+	D13 = 17 // PWM available
 )
 
 const (
@@ -32,6 +32,6 @@ const (
 
 // I2C pins
 const (
-	SDA_PIN = 22 // // SDA: SERCOM3/PAD[0]
-	SCL_PIN = 23 // // SCL: SERCOM3/PAD[1]
+	SDA_PIN = 22 // SDA: SERCOM3/PAD[0]
+	SCL_PIN = 23 // SCL: SERCOM3/PAD[1]
 )
