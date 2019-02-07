ba23d512c04a38455091cc0e8442144b2a3db467
Ron Evans <ron@hybridgroup.com>
2019-02-07 19:07:07 +0100

    cmd: improvements and simplifications to version subcommand
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- version.go
+++ version.go
@@ -1,15 +1,5 @@
 package main
 
-// tinyGoVersion of this package.
+// version of this package.
 // Update this value before release of new version of software.
-const tinyGoVersion = "0.1.0"
-
-// version returns the current TinyGo version
-func version() string {
-	return tinyGoVersion
-}
-
-// displayversion returns the current TinyGo full version description for display purposes.
-func displayversion() string {
-	return "TinyGo compiler v" + tinyGoVersion
-}
+const version = "0.1.0"
