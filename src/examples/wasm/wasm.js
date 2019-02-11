176b00f326b9f5b7bfbddb49110f21eda32b55a2
admin <rh.plus001@gmail.com>
2019-02-11 18:19:33 +0900

    support wasm example on Safari

--- src/examples/wasm/wasm.js
+++ src/examples/wasm/wasm.js
@@ -13,11 +13,22 @@ function init() {
   document.querySelector('#b').oninput = updateResult;
 
   const go = new Go();
-  WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then(function(obj) {
+  fetch(WASM_URL).then(resp =>
+    resp.arrayBuffer()
+  ).then(bytes =>
+    WebAssembly.instantiate(bytes, go.importObject).then(function(obj) {
+      wasm = obj.instance;
+      go.run(wasm);
+      updateResult();
+    })
+  )
+  // the following code is preferred but it doesn't work on Safari at this point.
+  // https://bugs.webkit.org/show_bug.cgi?id=173105
+  /*WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then(function(obj) {
     wasm = obj.instance;
     go.run(wasm);
     updateResult();
-  })
+  })*/
 }
 
 init();
