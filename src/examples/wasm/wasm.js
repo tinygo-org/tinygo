5ae3b7482050d7a7e75faba4a59bb8ade749d2b6
admin <rh.plus001@gmail.com>
2019-02-11 19:21:37 +0900

    fallback

--- src/examples/wasm/wasm.js
+++ src/examples/wasm/wasm.js
@@ -13,22 +13,23 @@ function init() {
   document.querySelector('#b').oninput = updateResult;
 
   const go = new Go();
-  fetch(WASM_URL).then(resp =>
-    resp.arrayBuffer()
-  ).then(bytes =>
-    WebAssembly.instantiate(bytes, go.importObject).then(function(obj) {
+  if ('instantiateStreaming' in WebAssembly) {
+    WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then(function(obj) {
       wasm = obj.instance;
       go.run(wasm);
       updateResult();
     })
-  )
-  // the following code is preferred but it doesn't work on Safari at this point.
-  // https://bugs.webkit.org/show_bug.cgi?id=173105
-  /*WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then(function(obj) {
-    wasm = obj.instance;
-    go.run(wasm);
-    updateResult();
-  })*/
+  } else {
+    fetch(WASM_URL).then(resp => 
+      resp.arrayBuffer()
+    ).then(bytes =>
+      WebAssembly.instantiate(bytes, go.importObject).then(function(obj) {
+        wasm = obj.instance;
+        go.run(wasm);
+        updateResult();
+      })
+    )
+  }
 }
 
 init();
