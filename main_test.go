9b4071237f91a47f1ff0507b9f58827c653cc0eb
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-02-14 11:40:27 +0100

    arm: switch to hardfloat ABI for Linux
    This avoids an error on the Raspberry Pi 3.

--- main_test.go
+++ main_test.go
@@ -68,7 +68,7 @@ func TestCompiler(t *testing.T) {
 				continue // TODO: improve CGo
 			}
 			t.Run(path, func(t *testing.T) {
-				runTest(path, tmpdir, "arm--linux-gnueabi", t)
+				runTest(path, tmpdir, "arm--linux-gnueabihf", t)
 			})
 		}
 
@@ -78,7 +78,7 @@ func TestCompiler(t *testing.T) {
 				continue // TODO: improve CGo
 			}
 			t.Run(path, func(t *testing.T) {
-				runTest(path, tmpdir, "aarch64--linux-gnueabi", t)
+				runTest(path, tmpdir, "aarch64--linux-gnu", t)
 			})
 		}
 	}
