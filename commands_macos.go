315cd4059fd38be4c078a5974556e5d120d1662a
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-04-07 15:40:03 +0200

    main: drop the dependency on llvm-ar
    The ar file format is pretty simple and can be implemented by using a Go
    library. Use that instead of calling out to llvm-ar.
    
    There are a few limitations to the used package, but that doesn't seem
    to matter for our use case (linking compiler-rt for use with ld.lld):
    
      * no index is created
      * long filenames are truncated
      * no support for archives bigger than 4GB

--- commands_macos.go
+++ commands_macos.go
@@ -4,7 +4,6 @@ package main
 
 // commands used by the compilation process might have different file names on macOS than those used on Linux.
 var commands = map[string]string{
-	"ar":      "llvm-ar",
 	"clang":   "clang-8",
 	"ld.lld":  "ld.lld",
 	"wasm-ld": "wasm-ld",
