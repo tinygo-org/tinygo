1d59a960bc326af3739348cc539982d4d2277b4b
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-04-27 17:31:50 +0200

    main: allow changing the clang command name

--- main.go
+++ main.go
@@ -211,7 +211,11 @@ func Compile(pkgName, outpath string, spec *TargetSpec, config *BuildConfig, act
 		// Compile extra files.
 		for i, path := range spec.ExtraFiles {
 			outpath := filepath.Join(dir, "extra-"+strconv.Itoa(i)+"-"+filepath.Base(path)+".o")
-			cmd := exec.Command(spec.Compiler, append(spec.CFlags, "-c", "-o", outpath, path)...)
+			cmdName := spec.Compiler
+			if name, ok := commands[cmdName]; ok {
+				cmdName = name
+			}
+			cmd := exec.Command(cmdName, append(spec.CFlags, "-c", "-o", outpath, path)...)
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			cmd.Dir = sourceDir()
@@ -227,7 +231,11 @@ func Compile(pkgName, outpath string, spec *TargetSpec, config *BuildConfig, act
 			for _, file := range pkg.CFiles {
 				path := filepath.Join(pkg.Package.Dir, file)
 				outpath := filepath.Join(dir, "pkg"+strconv.Itoa(i)+"-"+file+".o")
-				cmd := exec.Command(spec.Compiler, append(spec.CFlags, "-c", "-o", outpath, path)...)
+				cmdName := spec.Compiler
+				if name, ok := commands[cmdName]; ok {
+					cmdName = name
+				}
+				cmd := exec.Command(cmdName, append(spec.CFlags, "-c", "-o", outpath, path)...)
 				cmd.Stdout = os.Stdout
 				cmd.Stderr = os.Stderr
 				cmd.Dir = sourceDir()
