0f2eebe08e08e6b61fddd53b6c6cf56d28a5926d
Daniel Esteban <conejo@theninjabunny.com>
2019-05-05 11:49:09 +0200

    added back a simplified micro:bit matrix example

---
+++ src/examples/microbit-blink/microbit-blink.go
@@ -0,0 +1,24 @@
+// blink program for the BBC micro:bit
+package main
+
+import (
+	"machine"
+	"time"
+)
+
+// The LED matrix in the micro:bit is a multiplexed display: https://en.wikipedia.org/wiki/Multiplexed_display
+// Driver for easier control: https://github.com/tinygo-org/drivers/tree/master/microbitmatrix
+func main() {
+	ledrow := machine.GPIO{machine.LED_ROW_1}
+	ledrow.Configure(machine.GPIOConfig{Mode: machine.GPIO_OUTPUT})
+	ledcol := machine.GPIO{machine.LED_COL_1}
+	ledcol.Configure(machine.GPIOConfig{Mode: machine.GPIO_OUTPUT})
+	ledcol.Low()
+	for {
+		ledrow.Low()
+		time.Sleep(time.Millisecond * 500)
+
+		ledrow.High()
+		time.Sleep(time.Millisecond * 500)
+	}
+}
