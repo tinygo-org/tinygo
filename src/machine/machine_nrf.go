7075c842c1563b6bb4e5f6bc6022c6ee62ac9855
Anthony Elder <torntrousers@gmail.com>
2019-05-12 20:08:41 +0100

    Update SHORTS SUSPEND

--- src/machine/machine_nrf.go
+++ src/machine/machine_nrf.go
@@ -186,7 +186,7 @@ func (i2c I2C) Tx(addr uint16, w, r []byte) error {
 	}
 	if len(r) != 0 {
 		// To trigger suspend task when a byte is received
-		i2c.Bus.SHORTS &^= (nrf.TWI_SHORTS_BB_STOP << nrf.TWI_SHORTS_BB_STOP_Pos)
+		i2c.Bus.SHORTS = nrf.TWI_SHORTS_BB_SUSPEND
 		i2c.Bus.TASKS_STARTRX = 1 // re-start transmission for reading
 		for i := range r {        // read each char
 			// i2c.Bus.TASKS_RESUME = 1 // re-start transmission for reading
