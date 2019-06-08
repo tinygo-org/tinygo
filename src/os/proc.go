3e1c0d90c5de927d5ff5f334379b3734d3912021
Tomer Elmalem <telmalem@gmail.com>
2019-06-08 14:17:16 -0700

    Add implementation for os.Exit and syscall.Exit

---
+++ src/os/proc.go
@@ -0,0 +1,17 @@
+// Package os implements a subset of the Go "os" package. See
+// https://godoc.org/os for details.
+//
+// Note that the current implementation is blocking. This limitation should be
+// removed in a future version.
+package os
+
+import (
+	"syscall"
+)
+
+// Exit causes the current program to exit with the given status code.
+// Conventionally, code zero indicates success, non-zero an error.
+// The program terminates immediately; deferred functions are not run.
+func Exit(code int) {
+	syscall.Exit(code)
+}
---
+++ src/syscall/syscall.go
@@ -0,0 +1,3 @@
+package syscall
+
+func Exit(code int)
