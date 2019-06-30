d34bb7e7082225b9909e62601276055c1161c80f
Daniel Esteban <conejoninja@users.noreply.github.com>
2019-06-30 09:57:07 +0200

    add reelboard pins for the epaper display and the reset pin (#429)
    * machine/reelboard: added descriptive pin names for the epaper display and the reset pin

--- src/machine/board_reelboard.go
+++ src/machine/board_reelboard.go
@@ -6,15 +6,22 @@ const HasLowFrequencyCrystal = true
 
 // LEDs on the reel board
 const (
-	LED        Pin = LED1
-	LED1       Pin = LED_YELLOW
-	LED2       Pin = LED_RED
-	LED3       Pin = LED_GREEN
-	LED4       Pin = LED_BLUE
-	LED_RED    Pin = 11
-	LED_GREEN  Pin = 12
-	LED_BLUE   Pin = 41
-	LED_YELLOW Pin = 13
+	LED              Pin = LED1
+	LED1             Pin = LED_YELLOW
+	LED2             Pin = LED_RED
+	LED3             Pin = LED_GREEN
+	LED4             Pin = LED_BLUE
+	LED_RED          Pin = 11
+	LED_GREEN        Pin = 12
+	LED_BLUE         Pin = 41
+	LED_YELLOW       Pin = 13
+	EPD_BUSY_PIN     Pin = 14
+	EPD_RESET_PIN    Pin = 15
+	EPD_DC_PIN       Pin = 16
+	EPD_CS_PIN       Pin = 17
+	EPD_SCK_PIN      Pin = 19
+	EPD_MOSI_PIN     Pin = 20
+	POWER_SUPPLY_PIN Pin = 32
 )
 
 // User "a" button on the reel board
