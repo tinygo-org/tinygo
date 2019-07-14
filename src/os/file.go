b31fbbc2d2485ade7aaa5739bb954b9552139cb9
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-07-15 01:55:10 +0200

    os: gofmt

--- src/os/file.go
+++ src/os/file.go
@@ -145,12 +145,12 @@ const (
 
 // A FileInfo describes a file and is returned by Stat and Lstat.
 type FileInfo interface {
-	Name() string       // base name of the file
-	Size() int64        // length in bytes for regular files; system-dependent for others
-	Mode() FileMode     // file mode bits
+	Name() string   // base name of the file
+	Size() int64    // length in bytes for regular files; system-dependent for others
+	Mode() FileMode // file mode bits
 	// ModTime() time.Time // modification time
-	IsDir() bool        // abbreviation for Mode().IsDir()
-	Sys() interface{}   // underlying data source (can return nil)
+	IsDir() bool      // abbreviation for Mode().IsDir()
+	Sys() interface{} // underlying data source (can return nil)
 }
 
 // Stat is a stub, not yet implemented
