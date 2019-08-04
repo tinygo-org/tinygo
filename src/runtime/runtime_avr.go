10dc3c807a4796c85078ca4b27231c3d8c5e5347
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-08-04 15:32:33 +0200

    runtime: work around a bug in LLVM for .bss zeroing
    See the following bug: https://bugs.llvm.org/show_bug.cgi?id=42881
    
    I think this is a bug in LLVM, but the code in question wasn't the best
    code anyway. By fixing this, about 16 bytes of code are saved on ARM
    chips (and much more on AVR).

--- src/runtime/runtime_avr.go
+++ src/runtime/runtime_avr.go
@@ -47,10 +47,10 @@ func main() {
 
 func preinit() {
 	// Initialize .bss: zero-initialized global variables.
-	ptr := uintptr(unsafe.Pointer(&_sbss))
-	for ptr != uintptr(unsafe.Pointer(&_ebss)) {
-		*(*uint8)(unsafe.Pointer(ptr)) = 0
-		ptr += 1
+	ptr := unsafe.Pointer(&_sbss)
+	for ptr != unsafe.Pointer(&_ebss) {
+		*(*uint32)(ptr) = 0
+		ptr = unsafe.Pointer(uintptr(ptr) + 4)
 	}
 }
 
