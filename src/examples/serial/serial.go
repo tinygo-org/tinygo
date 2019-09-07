b180db509eb1fb8bce0f2101d2843f48404a7dcd
Paolo Oliveira <paolocmo@gmail.com>
2019-09-06 22:48:34 -0300

    working version of UART tested on Arduino

--- src/examples/serial/serial.go
+++ src/examples/serial/serial.go
@@ -1,10 +1,15 @@
 package main
 
-import "time"
+import (
+	"machine"
+	"time"
+)
 
 func main() {
+	serial := machine.UART0
+	serial.Configure(machine.UARTConfig{BaudRate: 9600})
 	for {
-		println("hello world!")
-		time.Sleep(time.Second)
+		serial.Write([]byte("hello tinyGo!\n"))
+		time.Sleep(time.Millisecond * 1000)
 	}
 }
