a5fb785334d8c0b31ccc4cfa92f4976a69bd8187
Ron Evans <ron@hybridgroup.com>
2019-09-18 10:43:20 +0200

    compiler: ensure that any passed in target, if it does not point to a .json file, is a full LLVM triple
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

---
+++ target_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestLoadTarget(t *testing.T) {
+	_, err := LoadTarget("arduino")
+	if err != nil {
+		t.Error("LoadTarget test failed:", err)
+	}
+
+	_, err = LoadTarget("notexist")
+	if err == nil {
+		t.Error("LoadTarget should have failed with non existing target")
+	}
+
+	if err.Error() != "expected a full LLVM target or a custom target in -target flag" {
+		t.Error("LoadTarget failed for wrong reason:", err)
+	}
+}
