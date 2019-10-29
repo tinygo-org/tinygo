3777791aa35cc1c825ce230edb30267b0d522d55
Ron Evans <ron@hybridgroup.com>
2019-10-29 18:59:54 +0100

    machine/samd51: allow setting pinmode for each of the SPI pins
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- src/machine/board_itsybitsy-m4.go
+++ src/machine/board_itsybitsy-m4.go
@@ -75,9 +75,11 @@ const (
 // SPI on the ItsyBitsy M4.
 var (
 	SPI0 = SPI{Bus: sam.SERCOM1_SPIM,
-		SCK:   SPI0_SCK_PIN,
-		MOSI:  SPI0_MOSI_PIN,
-		MISO:  SPI0_MISO_PIN,
-		DOpad: spiTXPad2SCK3,
-		DIpad: sercomRXPad0}
+		SCK:         SPI0_SCK_PIN,
+		MOSI:        SPI0_MOSI_PIN,
+		MISO:        SPI0_MISO_PIN,
+		DOpad:       spiTXPad2SCK3,
+		DIpad:       sercomRXPad3,
+		MISOPinMode: PinSERCOM,
+	}
 )
