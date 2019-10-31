59cc9013403746957e689f3c9ff3306879de0efb
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-10-31 16:38:49 +0100

    main: move compile options to compileopts package

---
+++ compileopts/options.go
@@ -0,0 +1,21 @@
+package compileopts
+
+// Options contains extra options to give to the compiler. These options are
+// usually passed from the command line.
+type Options struct {
+	Opt           string
+	GC            string
+	PanicStrategy string
+	Scheduler     string
+	PrintIR       bool
+	DumpSSA       bool
+	VerifyIR      bool
+	Debug         bool
+	PrintSizes    string
+	CFlags        []string
+	LDFlags       []string
+	Tags          string
+	WasmAbi       string
+	HeapSize      int64
+	TestConfig    TestConfig
+}
