6fd0c10e6fe10dc856de705ac8658b2a55815a4d
Scott Yoder <yodersb@gmail.com>
2020-02-07 22:45:14 -0500

    remove UART1 from board nrf52840-mdk to make things consistent
    ...and to remove confusion with the UARTE1 device.

--- src/machine/board_nrf52840-mdk.go
+++ src/machine/board_nrf52840-mdk.go
@@ -18,10 +18,9 @@ const (
 	UART_RX_PIN Pin = 19
 )
 
-// UART0 is the USB device and UART1 is the NRF UART
+// UART0 is the USB device
 var (
 	UART0 = USB
-	UART1 = NRF_UART0
 )
 
 // I2C pins (unused)
