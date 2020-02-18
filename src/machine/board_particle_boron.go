31f3e8155badb0fdd9d1e555a252be8b6a245312
Wojtek Siudzinski <admin@suda.pl>
2020-02-18 10:54:42 +0100

    Add the missing Pin type

--- src/machine/board_particle_boron.go
+++ src/machine/board_particle_boron.go
@@ -23,51 +23,51 @@ const (
 
 // I2C pins
 const (
-	SDA_PIN = 26
-	SCL_PIN = 27
+	SDA_PIN Pin = 26
+	SCL_PIN Pin = 27
 
 	// Internal I2C with MAX17043 and BQ24195 chips on it
-	SDA1_PIN = 24
-	SCL1_PIN = 41
-	INT1_PIN = 5
+	SDA1_PIN Pin = 24
+	SCL1_PIN Pin = 41
+	INT1_PIN Pin = 5
 )
 
 // SPI pins
 const (
-	SPI0_SCK_PIN  = 47
-	SPI0_MOSI_PIN = 45
-	SPI0_MISO_PIN = 46
+	SPI0_SCK_PIN  Pin = 47
+	SPI0_MOSI_PIN Pin = 45
+	SPI0_MISO_PIN Pin = 46
 )
 
 // Internal 4MB SPI Flash
 const (
-	SPI1_SCK_PIN  = 19
-	SPI1_MOSI_PIN = 20
-	SPI1_MISO_PIN = 21
-	SPI1_CS_PIN   = 17
-	SPI1_WP_PIN   = 22
-	SPI1_HOLD_PIN = 23
+	SPI1_SCK_PIN  Pin = 19
+	SPI1_MOSI_PIN Pin = 20
+	SPI1_MISO_PIN Pin = 21
+	SPI1_CS_PIN   Pin = 17
+	SPI1_WP_PIN   Pin = 22
+	SPI1_HOLD_PIN Pin = 23
 )
 
 // u-blox coprocessor
 const (
-	UBLOX_TXD_PIN      = 37
-	UBLOX_RXD_PIN      = 36
-	UBLOX_CTS_PIN      = 38
-	UBLOX_RTS_PIN      = 39
-	UBLOX_RESET_PIN    = 16
-	UBLOX_POWER_ON_PIN = 24
-	UBLOX_BUFF_EN_PIN  = 25
-	UBLOX_VINT_PIN     = 2
+	UBLOX_TXD_PIN      Pin = 37
+	UBLOX_RXD_PIN      Pin = 36
+	UBLOX_CTS_PIN      Pin = 38
+	UBLOX_RTS_PIN      Pin = 39
+	UBLOX_RESET_PIN    Pin = 16
+	UBLOX_POWER_ON_PIN Pin = 24
+	UBLOX_BUFF_EN_PIN  Pin = 25
+	UBLOX_VINT_PIN     Pin = 2
 )
 
 // Other periferals
 const (
-	MODE_BUTTON_PIN   = 11
-	CHARGE_STATUS_PIN = 41
-	LIPO_VOLTAGE_PIN  = 5
-	PCB_ANTENNA_PIN   = 2
-	EXTERNAL_UFL_PIN  = 25
-	NFC1_PIN          = 9
-	NFC2_PIN          = 10
+	MODE_BUTTON_PIN   Pin = 11
+	CHARGE_STATUS_PIN Pin = 41
+	LIPO_VOLTAGE_PIN  Pin = 5
+	PCB_ANTENNA_PIN   Pin = 2
+	EXTERNAL_UFL_PIN  Pin = 25
+	NFC1_PIN          Pin = 9
+	NFC2_PIN          Pin = 10
 )
