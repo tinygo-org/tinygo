478279c8f7856bb2aa4dca4e27c5befe0f6d7f8e
BCG <bgould@users.noreply.github.com>
2020-02-21 21:28:40 -0500

    incorporated code review feedback

--- src/machine/usb_nrf52840_reset_uf2.go
+++ src/machine/usb_nrf52840_reset_uf2.go
@@ -13,32 +13,34 @@ const (
 	DFU_MAGIC_OTA_RESET         = 0xA8
 )
 
+// checkShouldReset is called by the USB-CDC implementation to check whether to
+// reset into the bootloader/OTA and if so, resets the chip appropriately.
 func checkShouldReset() {
 	if usbLineInfo.dwDTERate == 1200 && usbLineInfo.lineState&usb_CDC_LINESTATE_DTR == 0 {
 		EnterUF2Bootloader()
 	}
 }
 
-// EnterSerialBootloader should perform a system reset in preparation
-// to switch to the bootloader to flash new firmware via serial/nrfutil
+// EnterSerialBootloader resets the chip into the serial bootloader. After
+// reset, it can be flashed using serial/nrfutil.
 func EnterSerialBootloader() {
 	arm.DisableInterrupts()
-	nrf.POWER.GPREGRET.Reg = uint32(DFU_MAGIC_SERIAL_ONLY_RESET)
+	nrf.POWER.GPREGRET.Set(DFU_MAGIC_SERIAL_ONLY_RESET)
 	arm.SystemReset()
 }
 
-// EnterUF2Bootloader should perform a system reset in preparation
-// to switch to the bootloader to flash new firmware via MSD/UF2
+// EnterUF2Bootloader resets the chip into the UF2 bootloader. After reset, it
+// can be flashed via nrfutil or by copying a UF2 file to the mass storage device
 func EnterUF2Bootloader() {
 	arm.DisableInterrupts()
-	nrf.POWER.GPREGRET.Reg = uint32(DFU_MAGIC_UF2_RESET)
+	nrf.POWER.GPREGRET.Set(DFU_MAGIC_UF2_RESET)
 	arm.SystemReset()
 }
 
-// EnterOTABootloader should perform a system reset in preparation
-// to switch to the bootloader to flash new firmware via OTA update
+// EnterOTABootloader resets the chip into the bootloader so that it can be
+// flashed via an OTA update
 func EnterOTABootloader() {
 	arm.DisableInterrupts()
-	nrf.POWER.GPREGRET.Reg = uint32(DFU_MAGIC_OTA_RESET)
+	nrf.POWER.GPREGRET.Set(DFU_MAGIC_OTA_RESET)
 	arm.SystemReset()
 }
