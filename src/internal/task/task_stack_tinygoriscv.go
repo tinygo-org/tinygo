878b62bbe8eb11a1783c722a730ea0ec9d8022ab
Ayke van Laethem <aykevanlaethem@gmail.com>
2020-03-17 20:45:30 +0100

    riscv: switch to tasks-based scheduler
    This is only supported for RV32 at the moment. RV64 can be added at a
    later time.

--- src/internal/task/task_stack_tinygoriscv.go
+++ src/internal/task/task_stack_tinygoriscv.go
@@ -1,4 +1,4 @@
-// +build scheduler.tasks,arm,!cortexm,!avr,!xtensa,!tinygo.riscv
+// +build scheduler.tasks,tinygo.riscv
 
 package task
 
@@ -7,17 +7,21 @@ import "unsafe"
 var systemStack uintptr
 
 // calleeSavedRegs is the list of registers that must be saved and restored when
-// switching between tasks. Also see task_stack_arm.S that relies on the exact
-// layout of this struct.
+// switching between tasks. Also see scheduler_riscv.S that relies on the
+// exact layout of this struct.
 type calleeSavedRegs struct {
-	r4  uintptr
-	r5  uintptr
-	r6  uintptr
-	r7  uintptr
-	r8  uintptr
-	r9  uintptr
-	r10 uintptr
-	r11 uintptr
+	s0  uintptr // x8 (fp)
+	s1  uintptr // x9
+	s2  uintptr // x18
+	s3  uintptr // x19
+	s4  uintptr // x20
+	s5  uintptr // x21
+	s6  uintptr // x22
+	s7  uintptr // x23
+	s8  uintptr // x24
+	s9  uintptr // x25
+	s10 uintptr // x26
+	s11 uintptr // x27
 
 	pc uintptr
 }
@@ -30,18 +34,19 @@ func (s *state) archInit(r *calleeSavedRegs, fn uintptr, args unsafe.Pointer) {
 	// Initialize the registers.
 	// These will be popped off of the stack on the first resume of the goroutine.
 
-	// Start the function at tinygo_startTask (defined in src/internal/task/task_stack_arm.S).
-	// This assembly code calls a function (passed in r4) with a single argument
-	// (passed in r5). After the function returns, it calls Pause().
+	// Start the function at tinygo_startTask (defined in src/internal/task/task_stack_riscv.S).
+	// This assembly code calls a function (passed in s0) with a single argument
+	// (passed in s1). After the function returns, it calls Pause().
 	r.pc = uintptr(unsafe.Pointer(&startTask))
 
-	// Pass the function to call in r4.
-	// This function is a compiler-generated wrapper which loads arguments out of a struct pointer.
-	// See createGoroutineStartWrapper (defined in compiler/goroutine.go) for more information.
-	r.r4 = fn
+	// Pass the function to call in s0.
+	// This function is a compiler-generated wrapper which loads arguments out
+	// of a struct pointer. See createGoroutineStartWrapper (defined in
+	// compiler/goroutine.go) for more information.
+	r.s0 = fn
 
-	// Pass the pointer to the arguments struct in r5.
-	r.r5 = uintptr(args)
+	// Pass the pointer to the arguments struct in s1.
+	r.s1 = uintptr(args)
 }
 
 func (s *state) resume() {
