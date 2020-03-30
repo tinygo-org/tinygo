6bcb40fe01d83451eafe42a195c4acf1b186b2dd
Ayke van Laethem <aykevanlaethem@gmail.com>
2020-03-30 20:52:52 +0200

    os: implement virtual filesystem support
    This allows applications to mount filesystems in the os package. This is
    useful for mounting external flash filesystems, for example.

--- src/os/file_other.go
+++ src/os/file_other.go
@@ -6,28 +6,44 @@ import (
 	_ "unsafe"
 )
 
+// Stdin, Stdout, and Stderr are open Files pointing to the standard input,
+// standard output, and standard error file descriptors.
+var (
+	Stdin  = &File{stdioFileHandle(0), "/dev/stdin"}
+	Stdout = &File{stdioFileHandle(1), "/dev/stdout"}
+	Stderr = &File{stdioFileHandle(2), "/dev/stderr"}
+)
+
+// isOS indicates whether we're running on a real operating system with
+// filesystem support.
+const isOS = false
+
+// stdioFileHandle represents one of stdin, stdout, or stderr depending on the
+// number. It implements the FileHandle interface.
+type stdioFileHandle uint8
+
 // Read is unsupported on this system.
-func (f *File) Read(b []byte) (n int, err error) {
-	return 0, errUnsupported
+func (f stdioFileHandle) Read(b []byte) (n int, err error) {
+	return 0, ErrUnsupported
 }
 
 // Write writes len(b) bytes to the output. It returns the number of bytes
 // written or an error if this file is not stdout or stderr.
-func (f *File) Write(b []byte) (n int, err error) {
-	switch f.fd {
-	case Stdout.fd, Stderr.fd:
+func (f stdioFileHandle) Write(b []byte) (n int, err error) {
+	switch f {
+	case 1, 2: // stdout, stderr
 		for _, c := range b {
 			putchar(c)
 		}
 		return len(b), nil
 	default:
-		return 0, errUnsupported
+		return 0, ErrUnsupported
 	}
 }
 
 // Close is unsupported on this system.
-func (f *File) Close() error {
-	return errUnsupported
+func (f stdioFileHandle) Close() error {
+	return ErrUnsupported
 }
 
 //go:linkname putchar runtime.putchar
---
+++ src/os/filesystem.go
@@ -0,0 +1,85 @@
+package os
+
+import (
+	"strings"
+)
+
+// mounts lists the mount points currently mounted in the filesystem provided by
+// the os package. To resolve a path to a mount point, it is scanned from top to
+// bottom looking for the first prefix match.
+var mounts []mountPoint
+
+type mountPoint struct {
+	// prefix is a filesystem prefix, that always starts and ends with a forward
+	// slash. To denote the root filesystem, use a single slash: "/".
+	// This allows fast checking whether a path lies within a mount point.
+	prefix string
+
+	// filesystem is the Filesystem implementation that is mounted at this mount
+	// point.
+	filesystem Filesystem
+}
+
+// Filesystem provides an interface for generic filesystem drivers mounted in
+// the os package. The errors returned must be one of the os.Err* errors, or a
+// custom error if one doesn't exist. It should not be a *PathError because
+// errors will be wrapped with a *PathError by the filesystem abstraction.
+//
+// WARNING: this interface is not finalized and may change in a future version.
+type Filesystem interface {
+	// OpenFile opens the named file.
+	OpenFile(name string, flag int, perm FileMode) (FileHandle, error)
+
+	// Mkdir creates a new directoy with the specified permission (before
+	// umask). Some filesystems may not support directories or permissions.
+	Mkdir(name string, perm FileMode) error
+
+	// Remove removes the named file or (empty) directory.
+	Remove(name string) error
+}
+
+// FileHandle is an interface that should be implemented by filesystems
+// implementing the Filesystem interface.
+//
+// WARNING: this interface is not finalized and may change in a future version.
+type FileHandle interface {
+	// Read reads up to len(b) bytes from the file.
+	Read(b []byte) (n int, err error)
+
+	// Write writes up to len(b) bytes to the file.
+	Write(b []byte) (n int, err error)
+
+	// Close closes the file, making it unusable for further writes.
+	Close() (err error)
+}
+
+// findMount returns the appropriate (mounted) filesystem to use for a given
+// filename plus the path relative to that filesystem.
+func findMount(path string) (Filesystem, string) {
+	for i := len(mounts) - 1; i >= 0; i-- {
+		mount := mounts[i]
+		if strings.HasPrefix(path, mount.prefix) {
+			return mount.filesystem, path[len(mount.prefix)-1:]
+		}
+	}
+	if isOS {
+		// Assume that the first entry in the mounts slice is the OS filesystem
+		// at the root of the directory tree. Use it as-is, to support relative
+		// paths.
+		return mounts[0].filesystem, path
+	}
+	return nil, path
+}
+
+// Mount mounts the given filesystem in the filesystem abstraction layer of the
+// os package. It is not possible to unmount filesystems. Filesystems added
+// later will override earlier filesystems.
+//
+// The provided prefix must start and end with a forward slash. This is true for
+// the root directory ("/") for example.
+func Mount(prefix string, filesystem Filesystem) {
+	if prefix[0] != '/' || prefix[len(prefix)-1] != '/' {
+		panic("os.Mount: invalid prefix")
+	}
+	mounts = append(mounts, mountPoint{prefix, filesystem})
+}
