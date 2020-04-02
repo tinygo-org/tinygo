f06d7d1bd6cf5d5231dde4de8ef4ae5ecd908074
Ayke van Laethem <aykevanlaethem@gmail.com>
2020-04-02 21:14:07 +0200

    builder: run tools (clang, ...) as separate processes
    This is necessary because LLVM defines many options in global variables
    that are modified when invoking Clang. In particular, LLVM 10 seems to
    have a bug in which it always sets the -pgo-warn-misexpect flag. Setting
    it multiple times (over various cc1 invocations) results in an error:
    
        clang (LLVM option parsing): for the --pgo-warn-misexpect option: may only occur zero or one times!
    
    This is fixed by running the Clang invocation in a new `tinygo`
    invocation.
    
    Because we've had issues with lld in the past, also run lld in a
    separate process so similar issues won't happen with lld in the future.

---
+++ builder/tools-builtin.go
@@ -0,0 +1,54 @@
+// +build byollvm
+
+package builder
+
+import (
+	"errors"
+	"unsafe"
+)
+
+/*
+#cgo CXXFLAGS: -fno-rtti
+#include <stdbool.h>
+#include <stdlib.h>
+bool tinygo_clang_driver(int argc, char **argv);
+bool tinygo_link_elf(int argc, char **argv);
+bool tinygo_link_wasm(int argc, char **argv);
+*/
+import "C"
+
+const hasBuiltinTools = true
+
+// RunTool runs the given tool (such as clang).
+//
+// This version actually runs the tools because TinyGo was compiled while
+// linking statically with LLVM (with the byollvm build tag).
+func RunTool(tool string, args ...string) error {
+	args = append([]string{"tinygo:" + tool}, args...)
+
+	var cflag *C.char
+	buf := C.calloc(C.size_t(len(args)), C.size_t(unsafe.Sizeof(cflag)))
+	defer C.free(buf)
+	cflags := (*[1 << 10]*C.char)(unsafe.Pointer(buf))[:len(args):len(args)]
+	for i, flag := range args {
+		cflag := C.CString(flag)
+		cflags[i] = cflag
+		defer C.free(unsafe.Pointer(cflag))
+	}
+
+	var ok C.bool
+	switch tool {
+	case "clang":
+		ok = C.tinygo_clang_driver(C.int(len(args)), (**C.char)(buf))
+	case "ld.lld":
+		ok = C.tinygo_link_elf(C.int(len(args)), (**C.char)(buf))
+	case "wasm-ld":
+		ok = C.tinygo_link_wasm(C.int(len(args)), (**C.char)(buf))
+	default:
+		return errors.New("unknown tool: " + tool)
+	}
+	if !ok {
+		return errors.New("failed to run tool: " + tool)
+	}
+	return nil
+}
---
+++ builder/tools-external.go
@@ -0,0 +1,15 @@
+// +build !byollvm
+
+package builder
+
+import "errors"
+
+const hasBuiltinTools = false
+
+// RunTool runs the given tool (such as clang).
+//
+// This version doesn't actually run the tool: TinyGo has not been compiled by
+// statically linking to LLVM.
+func RunTool(tool string, args ...string) error {
+	return errors.New("cannot run tool: " + tool)
+}
