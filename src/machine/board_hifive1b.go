2ef07c404b58efbad8f89e8039f7d8a94f409cb6
Yannis Huber <yannis.h@hotmail.ch>
2020-04-20 16:41:56 +0200

    Add PWM pin definitions

--- src/machine/board_hifive1b.go
+++ src/machine/board_hifive1b.go
@@ -11,10 +11,10 @@ const (
 	D5  = P21 // Blue LED/PWM (PWM1_PWM2)
 	D6  = P22 // Red LED/PWM (PWM1_PWM3)
 	D7  = P16
-	D8  = NoPin // PWM?
-	D9  = P01
-	D10 = P02   // SPI1_CS0
-	D11 = P03   // SPI1_DQ0
+	D8  = P00   // PWM (PWM0_PWM0)
+	D9  = P01   // PWM (PWM0_PWM1)
+	D10 = P02   // SPI1_CS0/PWM (PWM0_PWM2)
+	D11 = P03   // SPI1_DQ0/PWM (PWM0_PWM3)
 	D12 = P04   // SPI1_DQ1
 	D13 = P05   // SPI1_SCK
 	D14 = NoPin // not connected
@@ -56,4 +56,22 @@ const (
 const (
 	I2C0_SDA_PIN = D18
 	I2C0_SCL_PIN = D19
+)
+
+// PWM pins
+const (
+	PWM0_PWM0 = D8
+	PWM0_PWM1 = D9
+	PWM0_PWM2 = D10
+	PWM0_PWM3 = D11
+
+	PWM1_PWM0 = D4
+	PWM1_PWM1 = D3
+	PWM1_PWM2 = D5
+	PWM1_PWM3 = D6
+
+	PWM2_PWM0 = D16
+	PWM2_PWM1 = D17
+	PWM2_PWM2 = D18
+	PWM2_PWM3 = D19
 )
