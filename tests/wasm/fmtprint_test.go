16a7aecb9943d4108479c5bb8dc9f0662e8f58a1
Brad Peabody <brad@peabody.io>
2020-05-16 21:15:57 -0700

    debug output

--- tests/wasm/fmtprint_test.go
+++ tests/wasm/fmtprint_test.go
@@ -17,13 +17,17 @@ func TestFmtprint(t *testing.T) {
 	ctx, cancel := chromectx(5 * time.Second)
 	defer cancel()
 
+	var log1 string
 	err = chromedp.Run(ctx,
 		chromedp.Navigate("http://localhost:8826/run?file=fmtprint_pgm.wasm"),
+		chromedp.Sleep(time.Second),
+		chromedp.InnerHTML("#log", &log1),
 		waitLog(`test from fmtprint_pgm 1
 test from fmtprint_pgm 2
 test from fmtprint_pgm 3
 test from fmtprint_pgm 4`),
 	)
+	t.Logf("log1: %s", log1)
 	if err != nil {
 		t.Fatal(err)
 	}
