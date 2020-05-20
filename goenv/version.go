ab2a81cc5202e6fe130badf4ee4806614b21cba3
Ayke van Laethem <aykevanlaethem@gmail.com>
2020-05-21 00:34:01 +0200

    main: move TinyGo version to goenv
    This is needed to make it available to more packages, for caching
    purposes.
    
    For caching, the version itself may not be enough during development.
    But for regular releases, the version provides some protection against
    accidentally using a cache entry that is invalid in a newer version.

--- goenv/version.go
+++ goenv/version.go
@@ -10,6 +10,10 @@ import (
 	"strings"
 )
 
+// Version of TinyGo.
+// Update this value before release of new version of software.
+const Version = "0.14.0-dev"
+
 // GetGorootVersion returns the major and minor version for a given GOROOT path.
 // If the goroot cannot be determined, (0, 0) is returned.
 func GetGorootVersion(goroot string) (major, minor int, err error) {
--- version.go
+++
@@ -1,5 +0,0 @@
-package main
-
-// version of this package.
-// Update this value before release of new version of software.
-const version = "0.14.0-dev"
