ce5d51dc8620dc211896522e50488da940307c20
Brad Peabody <brad@peabody.io>
2020-05-23 11:35:37 -0700

    added test for wasm log output
    callback case for log test

---
+++ tests/wasm/log_test.go
@@ -0,0 +1,44 @@
+// +build go1.14
+
+package wasm
+
+import (
+	"testing"
+	"time"
+
+	"github.com/chromedp/chromedp"
+)
+
+func TestLog(t *testing.T) {
+
+	t.Parallel()
+
+	err := run("tinygo build -o " + wasmTmpDir + "/log.wasm -target wasm testdata/log.go")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx, cancel := chromectx(5 * time.Second)
+	defer cancel()
+
+	var log1 string
+	err = chromedp.Run(ctx,
+		chromedp.Navigate("http://localhost:8826/run?file=log.wasm"),
+		chromedp.Sleep(time.Second),
+		chromedp.InnerHTML("#log", &log1),
+		waitLogRe(`^..../../.. ..:..:.. log 1
+..../../.. ..:..:.. log 2
+..../../.. ..:..:.. log 3
+println 4
+fmt.Println 5
+..../../.. ..:..:.. log 6
+in func 1
+..../../.. ..:..:.. in func 2
+$`),
+	)
+	t.Logf("log1: %s", log1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
