c874cbeecf4f3b15154cf97add663c03afef259a
Yannis Huber <yannis.h@hotmail.ch>
2020-07-04 15:13:44 +0200

    runtime: reuse common code between 32 and 64-bit RISC-V

--- src/runtime/arch_tinygoriscv.go
+++ src/runtime/arch_tinygoriscv.go
@@ -1,18 +1,8 @@
-// +build tinygo.riscv32
+// +build tinygo.riscv
 
 package runtime
 
 import "device/riscv"
-
-const GOARCH = "arm" // riscv pretends to be arm
-
-// The bitness of the CPU (e.g. 8, 32, 64).
-const TargetBits = 32
-
-// Align on word boundary.
-func align(ptr uintptr) uintptr {
-	return (ptr + 3) &^ 3
-}
 
 func getCurrentStackPointer() uintptr {
 	return riscv.AsmFull("mv {}, sp", nil)
