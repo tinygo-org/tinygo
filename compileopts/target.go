af3e71e882aa050f8c5b6dade66bb3d83a98ba27
Jacques Supcik <jacques@supcik.net>
2020-08-10 09:17:25 +0200

    Add OpenOCD Commands

--- compileopts/target.go
+++ compileopts/target.go
@@ -49,6 +49,7 @@ type TargetSpec struct {
 	OpenOCDInterface string   `json:"openocd-interface"`
 	OpenOCDTarget    string   `json:"openocd-target"`
 	OpenOCDTransport string   `json:"openocd-transport"`
+	OpenOCDCommands  []string `json:"openocd-commands"`
 	JLinkDevice      string   `json:"jlink-device"`
 	CodeModel        string   `json:"code-model"`
 	RelocationModel  string   `json:"relocation-model"`
