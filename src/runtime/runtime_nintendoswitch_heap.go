0b9b2936518603b8bfa41b31f6ba95f3d511a018
Lucas Teske <lucas@teske.net.br>
2020-09-09 13:51:00 -0300

    nintendoswitch: Fix invalid memory read / write in print calls

--- src/runtime/runtime_nintendoswitch_heap.go
+++ src/runtime/runtime_nintendoswitch_heap.go
@@ -4,17 +4,11 @@
 
 package runtime
 
-import "unsafe"
-
 const heapSize = 0x2000000 * 16 // Default by libnx
-
-//go:extern _stack_top
-var stackTopSymbol [0]byte
 
 var (
 	heapStart = uintptr(0)
 	heapEnd   = uintptr(0)
-	stackTop  = uintptr(unsafe.Pointer(&stackTopSymbol))
 )
 
 //export setHeapSize
@@ -24,7 +18,7 @@ func preinit() {
 	setHeapSize(&heapStart, heapSize)
 
 	if heapStart == 0 {
-		panic("failed to allocate heap")
+		runtimePanic("failed to allocate heap")
 	}
 
 	heapEnd = heapStart + heapSize
