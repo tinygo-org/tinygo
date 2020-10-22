3ad216c62e874fccc743a7d3dd711708eefc82f3
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-10-22 19:24:30 +0200

    Update time functions

--- src/runtime/runtime_cosmwasm.go
+++ src/runtime/runtime_cosmwasm.go
@@ -6,7 +6,18 @@ import "unsafe"
 
 type timeUnit float64 // time in milliseconds, just like Date.now() in JavaScript
 
-const tickMicros = 1000000
+func ticksToNanoseconds(ticks timeUnit) int64 {
+	// The JavaScript API works in float64 milliseconds, so convert to
+	// nanoseconds first before converting to a timeUnit (which is a float64),
+	// to avoid precision loss.
+	return int64(ticks * 1e6)
+}
+
+func nanosecondsToTicks(ns int64) timeUnit {
+	// The JavaScript API works in float64 milliseconds, so convert to timeUnit
+	// (which is a float64) first before dividing, to avoid precision loss.
+	return timeUnit(ns) / 1e6
+}
 
 func postinit() {}
 
