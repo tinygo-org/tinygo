3e40b08ba005643bd0190fe45437a82404519c16
Ayke van Laethem <aykevanlaethem@gmail.com>
2020-10-28 00:49:13 +0100

    compiler: implement negate for complex numbers

--- testdata/float.go
+++ testdata/float.go
@@ -57,15 +57,17 @@ func main() {
 	println(complex64(c128))
 	println(complex128(c64))
 
-	// binops on complex numbers
+	// binops and negate on complex numbers
 	c64 = 5 + 2i
 	println("complex64 add: ", c64+-3+8i)
 	println("complex64 sub: ", c64 - -3 + 8i)
 	println("complex64 mul: ", c64*-3+8i)
 	println("complex64 div: ", c64/-3+8i)
+	println("complex64 neg: ", -c64)
 	c128 = -5 + 2i
 	println("complex128 add:", c128+2+6i)
 	println("complex128 sub:", c128-2+6i)
 	println("complex128 mul:", c128*2+6i)
 	println("complex128 div:", c128/2+6i)
+	println("complex128 neg:", -c128)
 }
