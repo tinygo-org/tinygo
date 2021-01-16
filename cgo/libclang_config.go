61c4f7c630a6c63ffa20653115bbefa91fd95b7d
Ayke van Laethem <aykevanlaethem@gmail.com>
2021-01-16 19:04:23 +0100

    main: use LLVM 11 by default when linking LLVM dynamically
    This doesn't affect the release builds but it is helpful for TinyGo
    developers.

--- cgo/libclang_config.go
+++ cgo/libclang_config.go
@@ -1,14 +1,14 @@
 // +build !byollvm
-// +build !llvm9,!llvm11
+// +build !llvm10,!llvm9
 
 package cgo
 
 /*
-#cgo linux  CFLAGS: -I/usr/lib/llvm-10/include
-#cgo darwin CFLAGS: -I/usr/local/opt/llvm@10/include
-#cgo freebsd CFLAGS: -I/usr/local/llvm10/include
-#cgo linux  LDFLAGS: -L/usr/lib/llvm-10/lib -lclang
-#cgo darwin LDFLAGS: -L/usr/local/opt/llvm@10/lib -lclang -lffi
-#cgo freebsd LDFLAGS: -L/usr/local/llvm10/lib -lclang
+#cgo linux   CFLAGS:  -I/usr/lib/llvm-11/include
+#cgo darwin  CFLAGS:  -I/usr/local/opt/llvm@11/include
+#cgo freebsd CFLAGS:  -I/usr/local/llvm11/include
+#cgo linux   LDFLAGS: -L/usr/lib/llvm-11/lib -lclang
+#cgo darwin  LDFLAGS: -L/usr/local/opt/llvm@11/lib -lclang -lffi
+#cgo freebsd LDFLAGS: -L/usr/local/llvm11/lib -lclang
 */
 import "C"
