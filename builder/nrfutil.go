1c55d9e89795e99295ab9277d73f103b2839a973
Olaf Flebbe <of@oflebbe.de>
2021-04-03 18:11:58 +0200

    support nrfutil zip format

---
+++ builder/nrfutil.go
@@ -0,0 +1,28 @@
+package builder
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+
+	"github.com/tinygo-org/tinygo/compileopts"
+)
+
+// https://infocenter.nordicsemi.com/index.jsp?topic=%2Fug_nrfutil%2FUG%2Fnrfutil%2Fnrfutil_intro.html
+
+func makeDFUFirmareImage(options *compileopts.Options, infile, outfile string) error {
+	cmdLine := []string{"nrfutil", "pkg", "generate", "--hw-version", "52", "--sd-req", "0x0", "--debug-mode", "--application", infile, outfile}
+
+	if options.PrintCommands {
+		fmt.Printf("%s\n ", strings.Join(cmdLine, " "))
+	}
+
+	cmd := exec.Command(cmdLine[0], cmdLine[1:]...)
+	cmd.Stdout = ioutil.Discard
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("could not run nrfutil pkg generate: %w", err)
+	}
+	return nil
+}
