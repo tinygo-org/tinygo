62012770efce48800cb7779d1795a1c9f9a3f15c
Kenneth Bell <ken@netleap.io>
2021-05-06 20:45:11 -0700

    stm32: add pwm for f4 series

---
+++ src/examples/pwm/stm32f4disco.go
@@ -0,0 +1,13 @@
+// +build stm32f4disco
+
+package main
+
+import "machine"
+
+var (
+	// These pins correspond to LEDs on the discovery
+	// board
+	pwm  = &machine.TIM4
+	pinA = machine.PD12
+	pinB = machine.PD13
+)
--- src/machine/machine_stm32f405.go
+++ src/machine/machine_stm32f405.go
@@ -13,6 +13,12 @@ func CPUFrequency() uint32 {
 	return 168000000
 }
 
+// Internal use: configured speed of the APB1 and APB2 timers, this should be kept
+// in sync with any changes to runtime package which configures the oscillators
+// and clock frequencies
+const APB1_TIM_FREQ = 42000000 * 2
+const APB2_TIM_FREQ = 84000000 * 2
+
 // Alternative peripheral pin functions
 const (
 	AF0_SYSTEM                = 0
