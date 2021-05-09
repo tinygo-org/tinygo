a7fd7f22cb81bac8ad9537d29cec184be30ac1d1
Kenneth Bell <ken@netleap.io>
2021-05-09 08:02:04 -0700

    stm32l4: add pwm

---
+++ src/examples/pwm/nucleo-l432kc.go
@@ -0,0 +1,11 @@
+// +build stm32l4
+
+package main
+
+import "machine"
+
+var (
+	pwm  = &machine.TIM2
+	pinA = machine.PA0
+	pinB = machine.PB3
+)
--- src/machine/machine_stm32l4x2.go
+++ src/machine/machine_stm32l4x2.go
@@ -12,6 +12,12 @@ func CPUFrequency() uint32 {
 	return 80000000
 }
 
+// Internal use: configured speed of the APB1 and APB2 timers, this should be kept
+// in sync with any changes to runtime package which configures the oscillators
+// and clock frequencies
+const APB1_TIM_FREQ = 80e6 // 80MHz
+const APB2_TIM_FREQ = 80e6 // 80MHz
+
 //---------- UART related code
 
 // Configure the UART.
