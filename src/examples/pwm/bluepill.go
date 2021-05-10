f05a9d20ac14e073c169d819e6ffdfab08d194b9
Kenneth Bell <ken@netleap.io>
2021-05-09 17:22:24 -0700

    stm32f103 (bluepill): add pwm

---
+++ src/examples/pwm/bluepill.go
@@ -0,0 +1,11 @@
+// +build bluepill
+
+package main
+
+import "machine"
+
+var (
+	pwm  = &machine.TIM2
+	pinA = machine.PA0
+	pinB = machine.PA1
+)
---
+++ src/machine/machine_stm32_tim_moder.go
@@ -0,0 +1,10 @@
+// +build stm32,!stm32f1
+
+package machine
+
+// Configuration of a GPIO pin for PWM output for STM32 MCUs with MODER
+// register (most MCUs except STM32F1 series).
+
+func (t *TIM) configurePin(channel uint8, pf PinFunction) {
+	pf.Pin.ConfigureAltFunc(PinConfig{Mode: PinModePWMOutput}, pf.AltFunc)
+}
