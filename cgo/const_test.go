6cdda2908040348b51fb2492063bd710eb484cb1
Ayke van Laethem <aykevanlaethem@gmail.com>
2021-05-20 14:25:25 +0200

    cgo: create skeleton of a Pratt parser
    This converts the existing const parser to the basics of a Pratt parser,
    following the book "Writing An Interpreter In Go" by Thorsten Ball. It
    doesn't really do anything interesting yet, it simply converts the
    existing code (with existing tests) to the new structure.

--- cgo/const_test.go
+++ cgo/const_test.go
@@ -18,7 +18,7 @@ func TestParseConst(t *testing.T) {
 		{`(5)`, `(5)`},
 		{`(((5)))`, `(5)`},
 		{`)`, `error: 1:1: unexpected token )`},
-		{`5)`, `error: 1:2: unexpected token )`},
+		{`5)`, `error: 1:2: unexpected token ), expected end of expression`},
 		{"  \t)", `error: 1:4: unexpected token )`},
 		{`5.8f`, `5.8`},
 		{`foo`, `C.foo`},
@@ -30,7 +30,7 @@ func TestParseConst(t *testing.T) {
 		{`'a'`, `'a'`},
 		{`0b10`, `0b10`},
 		{`0x1234_5678`, `0x1234_5678`},
-		{`5 5`, `error: 1:3: unexpected token INT`}, // test for a bugfix
+		{`5 5`, `error: 1:3: unexpected token INT, expected end of expression`}, // test for a bugfix
 	} {
 		fset := token.NewFileSet()
 		startPos := fset.AddFile("", -1, 1000).Pos(0)
