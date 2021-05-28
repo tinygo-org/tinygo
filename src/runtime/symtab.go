f7ce863bf4dea358a883fbfd95ec75b214a47051
Federico G. Schwindt <fgsch@lodoss.net>
2021-05-28 17:11:24 +0100

    Add runtime stubs required for net/http
    Continued from #1911.

---
+++ src/runtime/symtab.go
@@ -0,0 +1,20 @@
+package runtime
+
+type Frames struct {
+	//
+}
+
+type Frame struct {
+	Function string
+
+	File string
+	Line int
+}
+
+func CallersFrames(callers []uintptr) *Frames {
+	return nil
+}
+
+func (ci *Frames) Next() (frame Frame, more bool) {
+	return Frame{}, false
+}
