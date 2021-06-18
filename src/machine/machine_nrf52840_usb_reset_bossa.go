cd628bcde6d86cac40be31993bac4338120d75ff
Ayke van Laethem <aykevanlaethem@gmail.com>
2021-06-18 09:29:09 +0200

    nrf52840: add support for flashing with the BOSSA tool
    This only works with a custom bossac build from Arduino, not with the
    upstream version. It avoids needing the manual "double tap" to enter
    bootloader mode before flashing firmware.

---
+++ src/machine/machine_nrf52840_usb_reset_bossa.go
@@ -0,0 +1,26 @@
+// +build nrf52840,nrf52840_reset_bossa
+
+package machine
+
+import (
+	"device/arm"
+	"device/nrf"
+)
+
+const DFU_MAGIC_SERIAL_ONLY_RESET = 0xb0
+
+// checkShouldReset is called by the USB-CDC implementation to check whether to
+// reset into the bootloader/OTA and if so, resets the chip appropriately.
+func checkShouldReset() {
+	if usbLineInfo.dwDTERate == 1200 && usbLineInfo.lineState&usb_CDC_LINESTATE_DTR == 0 {
+		EnterSerialBootloader()
+	}
+}
+
+// EnterSerialBootloader resets the chip into the serial bootloader. After
+// reset, it can be flashed using serial/nrfutil.
+func EnterSerialBootloader() {
+	arm.DisableInterrupts()
+	nrf.POWER.GPREGRET.Set(DFU_MAGIC_SERIAL_ONLY_RESET)
+	arm.SystemReset()
+}
