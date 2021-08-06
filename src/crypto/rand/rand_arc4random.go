9d028dfc10eeb9a5ca2b6bc105f9076567ecbbf7
Ayke van Laethem <aykevanlaethem@gmail.com>
2021-08-06 21:00:51 +0200

    crypto/rand: switch to arc4random_buf
    This doesn't have the potential blocking issue of the getentropy call
    (which calls WASI random_get when using wasi-libc) and should therefore
    be a lot faster.
    
    For context, this is what the random_get documentation says:
    
    > Write high-quality random data into a buffer. This function blocks
    > when the implementation is unable to immediately provide sufficient
    > high-quality random data. This function may execute slowly, so when
    > large mounts of random data are required, it's advisable to use this
    > function to seed a pseudo-random number generator, rather than to
    > provide the random data directly.

---
+++ src/crypto/rand/rand_arc4random.go
@@ -0,0 +1,30 @@
+// +build darwin freebsd tinygo.wasm
+
+// This implementation of crypto/rand uses the arc4random_buf function
+// (available on both MacOS and WASI) to generate random numbers.
+//
+// Note: arc4random_buf (unlike what the name suggets) does not use the insecure
+// RC4 cipher. Instead, it uses a high-quality cipher, varying by the libc
+// implementation.
+
+package rand
+
+import "unsafe"
+
+func init() {
+	Reader = &reader{}
+}
+
+type reader struct {
+}
+
+func (r *reader) Read(b []byte) (n int, err error) {
+	if len(b) != 0 {
+		libc_arc4random_buf(unsafe.Pointer(&b[0]), uint(len(b)))
+	}
+	return len(b), nil
+}
+
+// void arc4random_buf(void *buf, size_t buflen);
+//export arc4random_buf
+func libc_arc4random_buf(buf unsafe.Pointer, buflen uint)
