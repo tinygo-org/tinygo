a628a8a6ddaf1c6d5a87e3ddf1c87abded14d498
Ayke van Laethem <aykevanlaethem@gmail.com>
2021-08-14 15:35:19 +0200

    all: add support for Go 1.17

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -23,7 +23,7 @@ import (
 // Version of the compiler pacakge. Must be incremented each time the compiler
 // package changes in a way that affects the generated LLVM module.
 // This version is independent of the TinyGo version number.
-const Version = 16 // last change: fix max slice size
+const Version = 17 // last change: add math.arch* aliases
 
 func init() {
 	llvm.InitializeAllTargets()
