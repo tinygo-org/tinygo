1e3f301e1d31d0f6dc8aa561359d6557016bf4a4
Ayke van Laethem <aykevanlaethem@gmail.com>
2021-08-15 02:03:45 +0200

    compiler: add *ssa.MakeSlice bounds tests
    There are some bugs in it. This commit adds the tests, so that the next
    commit can show what changed.

--- compiler/testdata/slice.go
+++ compiler/testdata/slice.go
@@ -23,3 +23,21 @@ func sliceAppendSlice(ints, added []int) []int {
 func sliceCopy(dst, src []int) int {
 	return copy(dst, src)
 }
+
+// Test bounds checking in *ssa.MakeSlice instruction.
+
+func makeByteSlice(len int) []byte {
+	return make([]byte, len)
+}
+
+func makeInt16Slice(len int) []int16 {
+	return make([]int16, len)
+}
+
+func makeArraySlice(len int) [][3]byte {
+	return make([][3]byte, len) // slice with element size of 3
+}
+
+func makeInt32Slice(len int) []int32 {
+	return make([]int32, len)
+}
