385c13e003af2ff36d82646a9cc0ad139d209923
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-08-21 17:16:59 +0200

    Better align cosmwasm with wasi

--- src/runtime/runtime_cosmwasm.go
+++ src/runtime/runtime_cosmwasm.go
@@ -4,22 +4,15 @@ package runtime
 
 import "unsafe"
 
-type timeUnit float64 // time in milliseconds, just like Date.now() in JavaScript
+type timeUnit int64 // time in milliseconds, just like Date.now() in JavaScript
 
 func ticksToNanoseconds(ticks timeUnit) int64 {
-	// The JavaScript API works in float64 milliseconds, so convert to
-	// nanoseconds first before converting to a timeUnit (which is a float64),
-	// to avoid precision loss.
-	return int64(ticks * 1e6)
+	return int64(ticks)
 }
 
 func nanosecondsToTicks(ns int64) timeUnit {
-	// The JavaScript API works in float64 milliseconds, so convert to timeUnit
-	// (which is a float64) first before dividing, to avoid precision loss.
-	return timeUnit(ns) / 1e6
+	return timeUnit(ns)
 }
-
-func postinit() {}
 
 //export _start
 func _start() {
@@ -30,19 +23,15 @@ func _start() {
 	run()
 }
 
-// putchar() is a no-op. Maybe we add an implementation later.
-// this is needed to link various debug print statements in tinygo
-func putchar(c byte) {}
-
 // This function is called by the scheduler.
 // Schedule a call to runtime.scheduler, do not actually sleep.
 //export runtime.sleepTicks
-func sleepTicks(d timeUnit)
+func sleepTicks(d timeUnit) {
+	// placeholder, should never be used
+}
 
 //export runtime.ticks
-func ticks() timeUnit
-
-// Abort executes the wasm 'unreachable' instruction.
-func abort() {
-	trap()
+func ticks() timeUnit {
+	// placeholder, should never be used
+	return 1234567890
 }
