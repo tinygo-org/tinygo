12333fefd33f00246782238d80b84ef0ac391237
Ayke van Laethem <aykevanlaethem@gmail.com>
2021-09-22 02:23:57 +0200

    arm: use armv7 instead of thumbv7
    At the moment, thumbv7 is crashing. I'm not exactly sure why, but it
    appears that there is an unknown instruction in __aeabi_uldivmod
    (probably from libgcc).
    
    I've fixed this by switching to armv7, which is also somewhat modern.
    Maybe we can switch back to Thumb2 (aka thumbv7) once we start using
    musl and compiler-rt. In the meantime, this does fix a miscompilation
    (illegal instruction).

--- compileopts/target.go
+++ compileopts/target.go
@@ -172,7 +172,7 @@ func LoadTarget(target string) (*TargetSpec, error) {
 			"386":   "i386",
 			"amd64": "x86_64",
 			"arm64": "aarch64",
-			"arm":   "thumbv7",
+			"arm":   "armv7",
 		}[goarch]
 		if llvmarch == "" {
 			llvmarch = goarch
