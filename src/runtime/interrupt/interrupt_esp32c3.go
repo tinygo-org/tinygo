936e0517baf3c6f1ea0cb1e0dc10004cb032e7d1
Dmitriy <mail@zdima.net>
2021-10-13 18:52:08 -0400

    mepc must be preserved and restored before mret. the mpie must be set before mret. remove memory allocation from interrupt code.

--- src/runtime/interrupt/interrupt_esp32c3.go
+++ src/runtime/interrupt/interrupt_esp32c3.go
@@ -52,10 +52,9 @@ func (i Interrupt) Enable() error {
 	mask := riscv.DisableInterrupts()
 	defer riscv.EnableInterrupts(mask)
 
-	intr := getInterruptForHandlerID(HandlerID(i.num))
-
-	// map mapRegister to intr
-	if err := HandlerID(i.num).set(intr); err != nil {
+	// map mapRegister to CPU interrupt
+	intr, err := HandlerID(i.num).set()
+	if err != nil {
 		return err
 	}
 
@@ -72,84 +71,47 @@ func (i Interrupt) Enable() error {
 	return nil
 }
 
-// getHandlerIDsForInterrupt return registered codes for interrupt
-func getHandlerIDsForInterrupt(interrupt int) []HandlerID {
-	switch interrupt {
-	case 1:
-		return []HandlerID{GPIO}
-	case 2:
-		return []HandlerID{UART0}
-	case 3:
-		return []HandlerID{UART1}
-	default:
-		return []HandlerID{
-			USB,
-			I2C0,
-			I2S1,
-			TIMG0,
-			TIMG1,
-			UHCI0,
-			RMT,
-			SPI1,
-			SPI2,
-			TWAI,
-			RNG,
-			WIFI,
-			BT,
-			WIFI_BT_COMMON,
-			BT_BASEBAND,
-			BT_LC,
-			RSA,
-			AES,
-			SHA,
-			HMAC,
-			DS,
-			GDMA,
-			SYSTIMER,
-			SARADC,
-		}
+var (
+	interruptMap = map[int][]HandlerID{
+		1: {GPIO},
+		2: {UART0},
+		3: {UART1},
 	}
-}
+)
 
-// getInterruptForHandlerID return registered codes for interrupt
-func getInterruptForHandlerID(ID HandlerID) int {
-	switch ID {
-	case GPIO:
-		return 1
-	case UART0:
-		return 2
-	case UART1:
-		return 3
-	default:
-		return 4
-	}
-}
-
-func (h HandlerID) set(interrupt int) error {
+// set will update and return module's CPU interrupt
+// IMPORTANT: must match interruptMap
+func (h HandlerID) set() (int, error) {
 	switch h {
-	case UART0:
-		esp.INTERRUPT_CORE0.UART_INTR_MAP.Set(uint32(interrupt))
-	case UART1:
-		esp.INTERRUPT_CORE0.UART1_INTR_MAP.Set(uint32(interrupt))
 	case GPIO:
-		esp.INTERRUPT_CORE0.GPIO_INTERRUPT_PRO_MAP.Set(uint32(interrupt))
+		esp.INTERRUPT_CORE0.GPIO_INTERRUPT_PRO_MAP.Set(1)
+		return 1, nil
+	case UART0:
+		esp.INTERRUPT_CORE0.UART_INTR_MAP.Set(2)
+		return 2, nil
+	case UART1:
+		esp.INTERRUPT_CORE0.UART1_INTR_MAP.Set(3)
+		return 3, nil
 	default:
-		return ErrInterruptNotImplemented
+		return -1, ErrInterruptNotImplemented
 	}
-	return nil
 }
 
 //export handleInterrupt
 func handleInterrupt() {
-	cause := riscv.MCAUSE.Get()
-	interruptNumber := uint32(cause & 0x1f)
+	mcause := riscv.MCAUSE.Get()
+	exception := mcause&(1<<31) == 0
+	interruptNumber := uint32(mcause & 0x1f)
 
-	if cause&(1<<31) != 0 && interruptNumber > 0 {
-		// disable this interrupt
+	if !exception && interruptNumber > 0 {
+		// save mepc, which could be overwritten by another CPU interrupt
+		mepc := riscv.MEPC.Get()
+
+		// disable interrupt
 		interruptBit := uint32(1 << interruptNumber)
 		esp.INTERRUPT_CORE0.CPU_INT_ENABLE.ClearBits(interruptBit)
 
-		// reset this interrupt
+		// reset pending status interrupt
 		if esp.INTERRUPT_CORE0.CPU_INT_TYPE.Get()&(interruptBit) != 0 {
 			// this is edge type interrupt
 			esp.INTERRUPT_CORE0.CPU_INT_CLEAR.SetBits(interruptBit)
@@ -162,31 +124,44 @@ func handleInterrupt() {
 		// enable CPU interrupts
 		riscv.MSTATUS.SetBits(0x8)
 
-		// Call the interrupt handler, if any is registered for this code.
-		for _, id := range getHandlerIDsForInterrupt(int(interruptNumber)) {
-			callInterruptHandler(int(id))
+		// Call registered interrupt handler(s)
+		if vector, ok := interruptMap[int(interruptNumber)]; ok {
+			for _, handlerID := range vector {
+				callInterruptHandler(int(handlerID))
+			}
+		} else {
+			println("unhandled CPU interrupt", interruptNumber)
 		}
 
 		// disable CPU interrupts
 		riscv.MSTATUS.ClearBits(0x8)
 
+		// mpie must be set to 1 to resume interrupts after 'MRET'
+		riscv.MSTATUS.SetBits(0x80)
+
+		// restore MEPC
+		riscv.MEPC.Set(mepc)
+
 		// enable this interrupt
 		esp.INTERRUPT_CORE0.CPU_INT_ENABLE.SetBits(interruptBit)
 
-		// enable CPU interrupts
-		riscv.MSTATUS.SetBits(0x8)
-
+		// do not enable CPU interrupts now
+		// the 'MRET' in src/device/riscv/handleinterrupt.S will copies the state of MPIE back into MIE, and subsequently clears MPIE.
+		// riscv.MSTATUS.SetBits(0x8)
 	} else {
 		// Topmost bit is clear, so it is an exception of some sort.
 		// We could implement support for unsupported instructions here (such as
 		// misaligned loads). However, for now we'll just print a fatal error.
-		handleException(interruptNumber)
+		handleException(mcause)
 	}
 }
 
 //export handleException
-func handleException(code uint32) {
-	println("*** Exception: code:", code)
+func handleException(mcause uintptr) {
+	// TODO need to get location of actual MEPC from the stack stash created in src/device/riscv/handleinterrupt.S
+	println("*** Exception:     pc:", riscv.MEPC.Get())
+	println("*** Exception:   code:", uint32(mcause&0x1f))
+	println("*** Exception: mcause:", mcause)
 	for {
 		riscv.Asm("wfi")
 	}
