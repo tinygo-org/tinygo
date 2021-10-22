5ed646a35207cedbc8d5bb3beb8cbfeddbf2057a
Dmitriy <mail@zdima.net>
2021-10-21 21:18:53 -0400

    assume the interrupt.Interrupt value is equal to CPU interrupt number.

--- src/runtime/interrupt/interrupt_esp32c3.go
+++ src/runtime/interrupt/interrupt_esp32c3.go
@@ -10,91 +10,40 @@ import (
 	"unsafe"
 )
 
-const (
-	// Each module of the ESP32C3 uses different interrupt registers.
-	// Below is the list of modules used from machine package to initialize interrupt handler
-	GPIO HandlerID = iota
-	UART0
-	UART1
-	USB
-	I2C0
-	I2S1
-	TIMG0
-	TIMG1
-	UHCI0
-	RMT
-	SPI1
-	SPI2
-	TWAI
-	RNG
-	WIFI
-	BT
-	WIFI_BT_COMMON
-	BT_BASEBAND
-	BT_LC
-	RSA
-	AES
-	SHA
-	HMAC
-	DS
-	GDMA
-	SYSTIMER
-	SARADC
-)
-
-var (
-	ErrInterruptNotImplemented = errors.New("interrupt number is not implemented")
-)
-
-type HandlerID int
-
+// Enable register CPU interrupt with interrupt.Interrupt.
+// The ESP32-C3 has 31 CPU independent interrupts.
+// The Interrupt.New(x, f) (x = [1..31]) attaches CPU interrupt to function f.
+// Caller must map the selected interrupt using following sequence (for example using id 5):
+//
+//    // map interrupt 5 to my XXXX module
+//    esp.INTERRUPT_CORE0.XXXX_INTERRUPT_PRO_MAP.Set( 5 )
+//    _ = Interrupt.New(5, func(interrupt.Interrupt) {
+//        ...
+//    }).Enable()
 func (i Interrupt) Enable() error {
+	if i.num < 1 && i.num > 31 {
+		return errors.New("interrupt for ESP32-C3 must be in range of 1 through 31")
+	}
 	mask := riscv.DisableInterrupts()
 	defer riscv.EnableInterrupts(mask)
 
-	// map mapRegister to CPU interrupt
-	intr, err := HandlerID(i.num).set()
-	if err != nil {
-		return err
-	}
+	// enable CPU interrupt number i.num
+	esp.INTERRUPT_CORE0.CPU_INT_ENABLE.SetBits(1 << i.num)
 
-	esp.INTERRUPT_CORE0.CPU_INT_ENABLE.SetBits(1 << intr)
-	esp.INTERRUPT_CORE0.CPU_INT_TYPE.SetBits(1 << intr)
-	// Set threshold to 5
-	reg := (*volatile.Register32)(unsafe.Pointer((uintptr(unsafe.Pointer(&esp.INTERRUPT_CORE0.CPU_INT_PRI_0)) + uintptr(intr)*4)))
+	// Set pulse interrupt type (rising edge detection)
+	esp.INTERRUPT_CORE0.CPU_INT_TYPE.SetBits(1 << i.num)
+
+	// Set default threshold to 5
+	reg := (*volatile.Register32)(unsafe.Pointer((uintptr(unsafe.Pointer(&esp.INTERRUPT_CORE0.CPU_INT_PRI_0)) + uintptr(i.num)*4)))
 	reg.Set(5)
 
-	esp.INTERRUPT_CORE0.CPU_INT_CLEAR.SetBits(1 << intr)
-	esp.INTERRUPT_CORE0.CPU_INT_CLEAR.ClearBits(1 << intr)
+	// Reset interrupt before reenabling
+	esp.INTERRUPT_CORE0.CPU_INT_CLEAR.SetBits(1 << i.num)
+	esp.INTERRUPT_CORE0.CPU_INT_CLEAR.ClearBits(1 << i.num)
 
+	// we must wait for any pending write operations to complete
 	riscv.Asm("fence")
 	return nil
-}
-
-var (
-	interruptMap = map[int][]HandlerID{
-		1: {GPIO},
-		2: {UART0},
-		3: {UART1},
-	}
-)
-
-// set will update and return module's CPU interrupt
-// IMPORTANT: must match interruptMap
-func (h HandlerID) set() (int, error) {
-	switch h {
-	case GPIO:
-		esp.INTERRUPT_CORE0.GPIO_INTERRUPT_PRO_MAP.Set(1)
-		return 1, nil
-	case UART0:
-		esp.INTERRUPT_CORE0.UART_INTR_MAP.Set(2)
-		return 2, nil
-	case UART1:
-		esp.INTERRUPT_CORE0.UART1_INTR_MAP.Set(3)
-		return 3, nil
-	default:
-		return -1, ErrInterruptNotImplemented
-	}
 }
 
 //export handleInterrupt
@@ -125,13 +74,7 @@ func handleInterrupt() {
 		riscv.MSTATUS.SetBits(0x8)
 
 		// Call registered interrupt handler(s)
-		if vector, ok := interruptMap[int(interruptNumber)]; ok {
-			for _, handlerID := range vector {
-				callInterruptHandler(int(handlerID))
-			}
-		} else {
-			println("unhandled CPU interrupt", interruptNumber)
-		}
+		callInterruptHandler(int(interruptNumber))
 
 		// disable CPU interrupts
 		riscv.MSTATUS.ClearBits(0x8)
