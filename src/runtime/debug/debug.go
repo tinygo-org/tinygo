44bb3812200998048aef95f313716d6908c045ed
Damian Gryski <damian@gryski.com>
2021-11-11 08:29:53 -0800

    src/runtime/debug: add SetMaxStack to allow compress/flate tests to build

--- src/runtime/debug/debug.go
+++ src/runtime/debug/debug.go
@@ -1,2 +1,10 @@
 // Package debug is a dummy package that is not yet implemented.
 package debug
+
+// SetMaxStack sets the maximum amount of memory that can be used by a single
+// goroutine stack.
+//
+// Not implemented.
+func SetMaxStack(n int) int {
+	return n
+}
