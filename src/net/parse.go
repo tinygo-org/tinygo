b70b6076e3fd228f66feb12f8ba6be82c4e7926b
Dan Kegel <dkegel@fastly.com>
2021-11-14 20:51:54 -0800

    net/ip, syscall/errno: Reduce code duplication by switching to internal/itoa.
    internal/itoa wasn't around back in go 1.12 days when tinygo's syscall/errno.go was written.
    It was only added as of go 1.17 ( https://github.com/golang/go/commit/061a6903a232cb868780b )
    so we have to have an internal copy for now.
    The internal copy should be deleted when tinygo drops support for go 1.16.
    
    FWIW, the new version seems nicer.
    It uses no allocations when converting 0,
    and although the optimizer might make this moot, uses
    a multiplication x 10 instead of a mod operation.

--- src/net/parse.go
+++ src/net/parse.go
@@ -64,24 +64,6 @@ func xtoi2(s string, e byte) (byte, bool) {
 	return byte(n), ok && ei == 2
 }
 
-// Convert unsigned integer to decimal string.
-func uitoa(val uint) string {
-	if val == 0 { // avoid string allocation
-		return "0"
-	}
-	var buf [20]byte // big enough for 64bit value base 10
-	i := len(buf) - 1
-	for val >= 10 {
-		q := val / 10
-		buf[i] = byte('0' + val - q*10)
-		i--
-		val = q
-	}
-	// val < 10
-	buf[i] = byte('0' + val)
-	return string(buf[i:])
-}
-
 // Convert i to a hexadecimal string. Leading zeros are not printed.
 func appendHex(dst []byte, i uint32) []byte {
 	if i == 0 {
--- src/syscall/str.go
+++ src/syscall/str.go
@@ -4,25 +4,6 @@
 
 package syscall
 
-func itoa(val int) string { // do it here rather than with fmt to avoid dependency
-	if val < 0 {
-		return "-" + uitoa(uint(-val))
-	}
-	return uitoa(uint(val))
-}
-
-func uitoa(val uint) string {
-	var buf [32]byte // big enough for int64
-	i := len(buf) - 1
-	for val >= 10 {
-		buf[i] = byte(val%10 + '0')
-		i--
-		val /= 10
-	}
-	buf[i] = byte(val + '0')
-	return string(buf[i:])
-}
-
 // clen returns the index of the first NULL byte in n or len(n) if n contains no NULL byte.
 func clen(n []byte) int {
 	for i := 0; i < len(n); i++ {
