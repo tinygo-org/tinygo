1d2c17753a9a7353a5ef753de7b002d5e625a6d3
Ayke van Laethem <aykevanlaethem@gmail.com>
2021-11-20 01:06:02 +0100

    tests: improve wasm tests slightly
    These wasm tests weren't passing in GitHub Actions and also weren't
    passing on my laptop. I'm not sure why, I think there are a few race
    conditions that are going on.
    
    This commit attempts to fix this at least to a degree:
    
      - The context deadline is increased from 5 seconds to 10 seconds.
      - The tests are not running in parallel anymore.
      - Some `Sleep` calls were removed, they do not appear to be necessary
        (and if they were, sleeping is the wrong solution to solve race
        conditions).
    
    Overall the tests are taking a few seconds more, but on the other hand
    they seem to be passing more reliable. At least for me, on my laptop
    (and hopefully also in CI).

--- tests/wasm/event_test.go
+++ tests/wasm/event_test.go
@@ -2,35 +2,30 @@ package wasm
 
 import (
 	"testing"
-	"time"
 
 	"github.com/chromedp/chromedp"
 )
 
 func TestEvent(t *testing.T) {
 
-	t.Parallel()
-
 	wasmTmpDir, server := startServer(t)
 
-	err := run("tinygo build -o " + wasmTmpDir + "/event.wasm -target wasm testdata/event.go")
+	err := run(t, "tinygo build -o "+wasmTmpDir+"/event.wasm -target wasm testdata/event.go")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	ctx, cancel := chromectx(5 * time.Second)
+	ctx, cancel := chromectx()
 	defer cancel()
 
 	var log1, log2 string
 	err = chromedp.Run(ctx,
 		chromedp.Navigate(server.URL+"/run?file=event.wasm"),
 		chromedp.WaitVisible("#log"),
-		chromedp.Sleep(time.Second),
 		chromedp.InnerHTML("#log", &log1),
 		waitLog(`1
 4`),
 		chromedp.Click("#testbtn"),
-		chromedp.Sleep(time.Second),
 		chromedp.InnerHTML("#log", &log2),
 		waitLog(`1
 4
