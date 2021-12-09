2eca84728892a37bc46f8caff101b015e8820a91
Dan Kegel <dkegel@fastly.com>
2021-12-08 19:10:09 -0800

    src/os/stat.go: get build tags right, maybe
    Should fix https://github.com/tinygo-org/tinygo/issues/2354
    
    Untested with wasi

---
+++ src/os/stat_other.go
@@ -0,0 +1,22 @@
+//  +build baremetal wasm,!wasi
+
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package os
+
+// Sync is a stub, not yet implemented
+func (f *File) Sync() error {
+	return ErrNotImplemented
+}
+
+// statNolog stats a file with no test logging.
+func statNolog(name string) (FileInfo, error) {
+	return nil, &PathError{Op: "stat", Path: name, Err: ErrNotImplemented}
+}
+
+// lstatNolog lstats a file with no test logging.
+func lstatNolog(name string) (FileInfo, error) {
+	return nil, &PathError{Op: "lstat", Path: name, Err: ErrNotImplemented}
+}
