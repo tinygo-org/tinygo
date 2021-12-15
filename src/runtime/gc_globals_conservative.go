531b7c92854e2757f78d200a4e8d48bf87dbc809
Nia Waldvogel <niaow1234@gmail.com>
2021-12-15 17:17:18 -0500

    runtime (gc): correct scan bounds
    This fixes 2 bugs in the GC scan bounds:
    1. On AVR, the GC could sometimes read one byte past the end of a block due to the difference between pointer size and alignment.
    2. On WASM, the linker does not properly align the marker for the end of the globals section. A manual alignment operation has been added to markGlobals to work around this.

--- src/runtime/gc_globals_conservative.go
+++ src/runtime/gc_globals_conservative.go
@@ -1,12 +1,24 @@
+//go:build (gc.conservative || gc.extalloc) && (baremetal || tinygo.wasm)
 // +build gc.conservative gc.extalloc
 // +build baremetal tinygo.wasm
 
 package runtime
+
+import "unsafe"
 
 // markGlobals marks all globals, which are reachable by definition.
 //
 // This implementation marks all globals conservatively and assumes it can use
 // linker-defined symbols for the start and end of the .data section.
 func markGlobals() {
-	markRoots(globalsStart, globalsEnd)
+	end := globalsEnd
+	if GOARCH == "wasm" {
+		// This is a workaround for a bug in wasm-ld: wasm-ld doesn't always
+		// align __heap_base and when this memory is shared through an API, it
+		// might result in unaligned memory. For details, see:
+		// https://reviews.llvm.org/D106499
+		// It should be removed once we switch to LLVM 13, where this is fixed.
+		end = end &^ (unsafe.Alignof(end) - 1)
+	}
+	markRoots(globalsStart, end)
 }
