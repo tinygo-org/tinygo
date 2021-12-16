3a7d3110b7af222f5fb8f45da63b0ae9dfb68bba
Dan Kegel <dkegel@fastly.com>
2021-12-16 13:08:57 -0800

    os: os_chmod_test.go: fix copyright

--- src/os/os_chmod_test.go
+++ src/os/os_chmod_test.go
@@ -1,6 +1,10 @@
 // +build !baremetal,!js,!wasi
 
-// TODO: Move this into os_test.go (as upstream has it) when wasi supports chmod
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// TODO: Move this back into os_test.go (as upstream has it) when wasi supports chmod
 
 package os_test
 
