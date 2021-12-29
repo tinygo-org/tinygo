b4eceafce71ba82a1e5cc80ac87a8fb23d964f5d
Pure White <wudi.daniel@bytedance.com>
2021-12-29 13:01:31 +0800

    feat: support build on darwin arm64

--- cgo/libclang_config.go
+++ cgo/libclang_config.go
@@ -1,14 +1,16 @@
-// +build !byollvm
-// +build !llvm12
+//go:build !byollvm && !llvm12
+// +build !byollvm,!llvm12
 
 package cgo
 
 /*
-#cgo linux   CFLAGS:  -I/usr/lib/llvm-11/include
-#cgo darwin  CFLAGS:  -I/usr/local/opt/llvm@11/include
-#cgo freebsd CFLAGS:  -I/usr/local/llvm11/include
-#cgo linux   LDFLAGS: -L/usr/lib/llvm-11/lib -lclang
-#cgo darwin  LDFLAGS: -L/usr/local/opt/llvm@11/lib -lclang -lffi
-#cgo freebsd LDFLAGS: -L/usr/local/llvm11/lib -lclang
+#cgo linux			CFLAGS:  -I/usr/lib/llvm-11/include
+#cgo darwin	amd64	CFLAGS:  -I/usr/local/opt/llvm@11/include
+#cgo darwin	ard64	CFLAGS:  -I/opt/homebrew/opt/llvm@11/include
+#cgo freebsd		CFLAGS:  -I/usr/local/llvm11/include
+#cgo linux			LDFLAGS: -L/usr/lib/llvm-11/lib -lclang
+#cgo darwin	amd64	LDFLAGS: -L/usr/local/opt/llvm@11/lib -lclang -lffi
+#cgo darwin	arm64	LDFLAGS: -L/opt/homebrew/opt/llvm@11/lib -lclang -lffi
+#cgo freebsd		LDFLAGS: -L/usr/local/llvm11/lib -lclang
 */
 import "C"
--- cgo/libclang_config_llvm12.go
+++ cgo/libclang_config_llvm12.go
@@ -1,14 +1,16 @@
-// +build !byollvm
-// +build llvm12
+//go:build !byollvm && llvm12
+// +build !byollvm,llvm12
 
 package cgo
 
 /*
-#cgo linux   CFLAGS:  -I/usr/lib/llvm-12/include
-#cgo darwin  CFLAGS:  -I/usr/local/opt/llvm@12/include
-#cgo freebsd CFLAGS:  -I/usr/local/llvm12/include
-#cgo linux   LDFLAGS: -L/usr/lib/llvm-12/lib -lclang
-#cgo darwin  LDFLAGS: -L/usr/local/opt/llvm@12/lib -lclang -lffi
-#cgo freebsd LDFLAGS: -L/usr/local/llvm12/lib -lclang
+#cgo linux			CFLAGS:  -I/usr/lib/llvm-12/include
+#cgo darwin	amd64	CFLAGS:  -I/usr/local/opt/llvm@12/include
+#cgo darwin	arm64	CFLAGS:  -I/opt/homebrew/opt/llvm@12/include
+#cgo freebsd		CFLAGS:  -I/usr/local/llvm12/include
+#cgo linux			LDFLAGS: -L/usr/lib/llvm-12/lib -lclang
+#cgo darwin	amd64	LDFLAGS: -L/usr/local/opt/llvm@12/lib -lclang -lffi
+#cgo darwin	arm64	LDFLAGS: -L/opt/homebrew/opt/llvm@12/lib -lclang -lffi
+#cgo freebsd		LDFLAGS: -L/usr/local/llvm12/lib -lclang
 */
 import "C"
