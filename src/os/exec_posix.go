db8603e37809ef816300f9f77787bae28098ced1
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2022-02-05 06:18:33 -0500

    Add os Signal aliases
    These are used in Go 1.18's `testing/internal/testdeps`. Though the
    comment says they should exist _everywhere_, there is still a build
    constraint, but that seems to be fine.

---
+++ src/os/exec_posix.go
@@ -0,0 +1,22 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package os
+
+//go:build aix || darwin || dragonfly || freebsd || (js && wasm) || linux || netbsd || openbsd || solaris || windows
+// +build aix darwin dragonfly freebsd js,wasm linux netbsd openbsd solaris windows
+
+import (
+	"syscall"
+)
+
+// The only signal values guaranteed to be present in the os package on all
+// systems are os.Interrupt (send the process an interrupt) and os.Kill (force
+// the process to exit). On Windows, sending os.Interrupt to a process with
+// os.Process.Signal is not implemented; it will return an error instead of
+// sending a signal.
+var (
+	Interrupt Signal = syscall.SIGINT
+	Kill      Signal = syscall.SIGKILL
+)
