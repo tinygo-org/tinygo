881aba17853e1a1870c14edf1f2f212fd869d224
Aayush Attri <aayush.attri@snyk.io>
2022-03-12 12:12:16 +0000

    add stub for runtime numcgocall, numgoroutine and version

--- src/runtime/debug.go
+++ src/runtime/debug.go
@@ -8,3 +8,17 @@ package runtime
 func NumCPU() int {
 	return 1
 }
+
+// NumCgoCall returns the number of cgo calls made by the current process.
+func NumCgoCall() int {
+	return 0
+}
+
+// NumGoroutine returns the number of goroutines that currently exist.
+func NumGoroutine() int {
+	return 1
+}
+
+func Version() string {
+	return "v0.1.0"
+}
