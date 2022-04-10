0590ccb2602a1acc281d21d9a43192544be96250
Dan Kegel <dkegel@fastly.com>
2022-04-09 18:54:28 -0700

    gofmt exec_posix.go

--- src/os/exec_posix.go
+++ src/os/exec_posix.go
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package os
-
 //go:build aix || darwin || dragonfly || freebsd || (js && wasm) || linux || netbsd || openbsd || solaris || windows
 // +build aix darwin dragonfly freebsd js,wasm linux netbsd openbsd solaris windows
+
+package os
 
 import (
 	"syscall"
