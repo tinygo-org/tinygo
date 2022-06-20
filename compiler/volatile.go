1ceb63d14c69c013fb25d8ed09cbe45658ecf3ad
Ayke van Laethem <aykevanlaethem@gmail.com>
2022-06-20 14:56:55 +0200

    compiler: really define runtime/volatile.* functions
    This makes them available to deferred calls, among others.

--- compiler/volatile.go
+++ compiler/volatile.go
@@ -3,28 +3,25 @@ package compiler
 // This file implements volatile loads/stores in runtime/volatile.LoadT and
 // runtime/volatile.StoreT as compiler builtins.
 
-import (
-	"golang.org/x/tools/go/ssa"
-	"tinygo.org/x/go-llvm"
-)
-
 // createVolatileLoad is the implementation of the intrinsic function
 // runtime/volatile.LoadT().
-func (b *builder) createVolatileLoad(instr *ssa.CallCommon) (llvm.Value, error) {
-	addr := b.getValue(instr.Args[0])
-	b.createNilCheck(instr.Args[0], addr, "deref")
+func (b *builder) createVolatileLoad() {
+	b.createFunctionStart()
+	addr := b.getValue(b.fn.Params[0])
+	b.createNilCheck(b.fn.Params[0], addr, "deref")
 	val := b.CreateLoad(addr, "")
 	val.SetVolatile(true)
-	return val, nil
+	b.CreateRet(val)
 }
 
 // createVolatileStore is the implementation of the intrinsic function
 // runtime/volatile.StoreT().
-func (b *builder) createVolatileStore(instr *ssa.CallCommon) (llvm.Value, error) {
-	addr := b.getValue(instr.Args[0])
-	val := b.getValue(instr.Args[1])
-	b.createNilCheck(instr.Args[0], addr, "deref")
+func (b *builder) createVolatileStore() {
+	b.createFunctionStart()
+	addr := b.getValue(b.fn.Params[0])
+	val := b.getValue(b.fn.Params[1])
+	b.createNilCheck(b.fn.Params[0], addr, "deref")
 	store := b.CreateStore(val, addr)
 	store.SetVolatile(true)
-	return llvm.Value{}, nil
+	b.CreateRetVoid()
 }
