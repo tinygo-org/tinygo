153ff09cc5b2d3986127ad0f7f72f2818e3f5f66
Ayke van Laethem <aykevanlaethem@gmail.com>
2022-06-28 16:14:33 +0200

    esp8266: provide hardware pin constants like GPIO2

--- src/machine/board_nodemcu.go
+++ src/machine/board_nodemcu.go
@@ -7,15 +7,15 @@ package machine
 
 // GPIO pins on the NodeMCU board.
 const (
-	D0 Pin = 16
-	D1 Pin = 5
-	D2 Pin = 4
-	D3 Pin = 0
-	D4 Pin = 2
-	D5 Pin = 14
-	D6 Pin = 12
-	D7 Pin = 13
-	D8 Pin = 15
+	D0 = GPIO16
+	D1 = GPIO5
+	D2 = GPIO4
+	D3 = GPIO0
+	D4 = GPIO2
+	D5 = GPIO14
+	D6 = GPIO12
+	D7 = GPIO13
+	D8 = GPIO15
 )
 
 // Onboard blue LED (on the AI-Thinker module).
