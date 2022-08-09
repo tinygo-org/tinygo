bb6cb81ed0ea07618aa4451d315ae1acb05fc1cb
Damian Gryski <damian@gryski.com>
2022-08-09 14:03:19 -0700

    runtime: add MemStats.TotalAlloc

--- src/runtime/mstats_leaking.go
+++ src/runtime/mstats_leaking.go
@@ -9,10 +9,11 @@ package runtime
 // call to ReadMemStats. This would not do GC implicitly for you.
 func ReadMemStats(m *MemStats) {
 	m.HeapIdle = 0
-	m.HeapInuse = uint64(heapptr - heapStart)
+	m.HeapInuse = gcTotalAlloc
 	m.HeapReleased = 0 // always 0, we don't currently release memory back to the OS.
 
 	m.HeapSys = m.HeapInuse + m.HeapIdle
 	m.GCSys = 0
+	m.TotalAlloc = gcTotalAlloc
 	m.Sys = uint64(heapEnd - heapStart)
 }
