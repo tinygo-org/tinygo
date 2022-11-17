67841207e89f9e10d0edca0b66de6427b802fded
Ayke van Laethem <aykevanlaethem@gmail.com>
2022-11-17 14:22:47 +0100

    Revert "Enable wasm pointer tracking for gc=none."
    This reverts commit 0b3a7280fa7fd9cf479a6ebcf608c40280d86b9f and updates
    the documentation a little bit to explain the purpose of -gc=none. (I'm
    thinking about the attiny10 by the way where defaulting to -gc=none
    makes sense).

--- compileopts/config.go
+++ compileopts/config.go
@@ -105,8 +105,6 @@ func (c *Config) GC() string {
 // that can be traced by the garbage collector.
 func (c *Config) NeedsStackObjects() bool {
 	switch c.GC() {
-	case "none":
-		fallthrough
 	case "conservative":
 		for _, tag := range c.BuildTags() {
 			if tag == "tinygo.wasm" {
