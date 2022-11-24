49007891c893f3d7b493995dab01826cc1036fd1
Ayke van Laethem <aykevanlaethem@gmail.com>
2022-11-24 21:06:37 +0100

    all: re-enable AVR tests
    I have some confidence they'll work reliably now.

--- main_test.go
+++ main_test.go
@@ -134,10 +134,7 @@ func TestBuild(t *testing.T) {
 	})
 
 	t.Run("AVR", func(t *testing.T) {
-		// LLVM backend crash:
-		// LIBCLANG FATAL ERROR: Cannot select: t3: i16 = JumpTable<0>
-		// This bug is non-deterministic.
-		t.Skip("skipped due to non-deterministic backend bugs")
+		t.Parallel()
 		runPlatTests(optionsFromTarget("simavr", sema), tests, t)
 	})
 
