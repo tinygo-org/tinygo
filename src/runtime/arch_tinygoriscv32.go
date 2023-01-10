7fc9fe2fe3dc17f51f5d1c1113c7c2c49189e56f
Ayke van Laethem <aykevanlaethem@gmail.com>
2023-01-10 20:55:29 +0100

    riscv: use 16-byte alignment everywhere
    Source: https://riscv.org/wp-content/uploads/2015/01/riscv-calling.pdf

--- src/runtime/arch_tinygoriscv32.go
+++ src/runtime/arch_tinygoriscv32.go
@@ -6,8 +6,3 @@ const GOARCH = "arm" // riscv pretends to be arm
 
 // The bitness of the CPU (e.g. 8, 32, 64).
 const TargetBits = 32
-
-// Align on word boundary.
-func align(ptr uintptr) uintptr {
-	return (ptr + 3) &^ 3
-}
