483406c23b8d2de95f2cc212a26062ddc0a4a7e3
deadprogram <ron@hybridgroup.com>
2023-01-11 14:41:34 +0100

    machine/rp2040: implement Flash interface
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- src/machine/flash.go
+++ src/machine/flash.go
@@ -1,4 +1,4 @@
-//go:build nrf || nrf51 || nrf52 || nrf528xx || stm32f4 || stm32l4 || stm32wlx || atsamd21 || atsamd51 || atsame5x
+//go:build nrf || nrf51 || nrf52 || nrf528xx || stm32f4 || stm32l4 || stm32wlx || atsamd21 || atsamd51 || atsame5x || rp2040
 
 package machine
 
