dfbda6d409d353ae822a72c861352efbc039e8cd
Justin A. Wilson <justin.wilson@omibyte.io>
2023-02-05 17:26:51 -0600

    Added missing TCPAddr and UDPAddr implementations to the net package

--- src/net/udpsock.go
+++ src/net/udpsock.go
@@ -5,19 +5,19 @@ import (
 	"net/netip"
 )
 
-// TCPAddr represents the address of a TCP end point.
-type TCPAddr struct {
+// UDPAddr represents the address of a UDP end point.
+type UDPAddr struct {
 	IP   IP
 	Port int
 	Zone string // IPv6 scoped addressing zone
 }
 
-// AddrPort returns the TCPAddr a as a netip.AddrPort.
+// AddrPort returns the UDPAddr a as a netip.AddrPort.
 //
 // If a.Port does not fit in a uint16, it's silently truncated.
 //
 // If a is nil, a zero value is returned.
-func (a *TCPAddr) AddrPort() netip.AddrPort {
+func (a *UDPAddr) AddrPort() netip.AddrPort {
 	if a == nil {
 		return netip.AddrPort{}
 	}
@@ -26,10 +26,10 @@ func (a *TCPAddr) AddrPort() netip.AddrPort {
 	return netip.AddrPortFrom(na, uint16(a.Port))
 }
 
-// Network returns the address's network name, "tcp".
-func (a *TCPAddr) Network() string { return "tcp" }
+// Network returns the address's network name, "udp".
+func (a *UDPAddr) Network() string { return "udp" }
 
-func (a *TCPAddr) String() string {
+func (a *UDPAddr) String() string {
 	if a == nil {
 		return "<nil>"
 	}
@@ -40,26 +40,16 @@ func (a *TCPAddr) String() string {
 	return JoinHostPort(ip, itoa.Itoa(a.Port))
 }
 
-func (a *TCPAddr) isWildcard() bool {
+func (a *UDPAddr) isWildcard() bool {
 	if a == nil || a.IP == nil {
 		return true
 	}
 	return a.IP.IsUnspecified()
 }
 
-func (a *TCPAddr) opAddr() Addr {
+func (a *UDPAddr) opAddr() Addr {
 	if a == nil {
 		return nil
 	}
 	return a
-}
-
-// TCPConn is an implementation of the Conn interface for TCP network
-// connections.
-type TCPConn struct {
-	conn
-}
-
-func (c *TCPConn) CloseWrite() error {
-	return &OpError{"close", "", nil, nil, ErrNotImplemented}
 }
