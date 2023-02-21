b4d56bfa5e8dff23501dc32fe2aa3ec3d97d7fa8
deadprogram <ron@hybridgroup.com>
2023-02-21 23:42:25 +0100

    machine/flash: adding some hopefully helpful comments
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- src/machine/flash.go
+++ src/machine/flash.go
@@ -13,7 +13,7 @@ var flashDataEnd [0]byte
 // Return the start of the writable flash area, aligned on a page boundary. This
 // is usually just after the program and static data.
 func FlashDataStart() uintptr {
-	return (uintptr(unsafe.Pointer(&flashDataStart)) + flashPageSize - 1) &^ (flashPageSize - 1)
+	return (uintptr(unsafe.Pointer(&flashDataStart)) + FlashPageSize - 1) &^ (FlashPageSize - 1)
 }
 
 // Return the end of the writable flash area. Usually this is the address one
@@ -22,22 +22,26 @@ func FlashDataEnd() uintptr {
 	return uintptr(unsafe.Pointer(&flashDataEnd))
 }
 
+// Flasher interface is what a processor needs to implement for Flash API.
 type Flasher interface {
 	ErasePage(address uintptr) error
 	WriteData(address uintptr, data []byte) error
 	ReadData(address uintptr, data []byte) (n int, err error)
 }
 
+// FlashBuffer implements the ReadWriteCloser interface using the Flasher interface.
 type FlashBuffer struct {
 	f       Flasher
 	start   uintptr
 	current uintptr
 }
 
+// OpenFlashBuffer opens a FlashBuffer.
 func OpenFlashBuffer(f Flasher, address uintptr) *FlashBuffer {
 	return &FlashBuffer{f: f, start: address, current: address}
 }
 
+// Read data from a FlashBuffer.
 func (fl *FlashBuffer) Read(p []byte) (n int, err error) {
 	fl.f.ReadData(fl.current, p)
 
@@ -46,6 +50,7 @@ func (fl *FlashBuffer) Read(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+// Write data to a FlashBuffer.
 func (fl *FlashBuffer) Write(p []byte) (n int, err error) {
 	// any new pages needed?
 	currentPageCount := (fl.current - fl.start + FlashPageSize - 1) / FlashPageSize
@@ -60,6 +65,7 @@ func (fl *FlashBuffer) Write(p []byte) (n int, err error) {
 	return 0, nil
 }
 
+// Close the FlashBuffer.
 func (fl *FlashBuffer) Close() error {
 	return nil
 }
