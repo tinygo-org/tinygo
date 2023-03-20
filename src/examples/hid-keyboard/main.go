a4a1001dd39755eb174522be2ca83857ac3c101c
deadprogram <ron@hybridgroup.com>
2023-03-21 00:10:44 +0100

    examples: use hid-keyboard example to show how to to override default USB VID, PID, manufacturer name, and product name
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- src/examples/hid-keyboard/main.go
+++ src/examples/hid-keyboard/main.go
@@ -1,10 +1,21 @@
+// to override the USB Manufacturer or Product names:
+//
+// tinygo flash -target circuitplay-express -ldflags="-X main.usbManufacturer='TinyGopher Labs' -X main.usbProduct='GopherKeyboard'" examples/hid-keyboard
+//
+// you can also override the VID/PID. however, only set this if you know what you are doing,
+// since changing it can make it difficult to reflash some devices.
 package main
 
 import (
 	"machine"
+	"machine/usb"
 	"machine/usb/hid/keyboard"
+	"strconv"
 	"time"
 )
+
+var usbVID, usbPID string
+var usbManufacturer, usbProduct string
 
 func main() {
 	button := machine.BUTTON
@@ -17,5 +28,25 @@ func main() {
 			kb.Write([]byte("tinygo"))
 			time.Sleep(200 * time.Millisecond)
 		}
+	}
+}
+
+func init() {
+	if usbVID != "" {
+		vid, _ := strconv.ParseUint(usbVID, 0, 16)
+		usb.VendorID = uint16(vid)
+	}
+
+	if usbPID != "" {
+		pid, _ := strconv.ParseUint(usbPID, 0, 16)
+		usb.ProductID = uint16(pid)
+	}
+
+	if usbManufacturer != "" {
+		usb.Manufacturer = usbManufacturer
+	}
+
+	if usbProduct != "" {
+		usb.Product = usbProduct
 	}
 }
