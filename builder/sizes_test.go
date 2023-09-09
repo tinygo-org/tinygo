4c3c6216c81d19621d6b3d975c2b678ac9bb80fd
Filip Vranesevic <filip031@gmail.com>
2023-09-09 22:43:42 +0200

    Simplify algorithm

--- builder/sizes_test.go
+++ builder/sizes_test.go
@@ -41,9 +41,8 @@ func TestBinarySize(t *testing.T) {
 	// This is a small number of very diverse targets that we want to test.
 	tests := []sizeTest{
 		// microcontrollers
-
-		{"hifive1b", "examples/echo", 4720, 280, 0, 2252},
-		{"microbit", "examples/serial", 2820, 388, 8, 2256},
+		{"hifive1b", "examples/echo", 4716, 280, 0, 2252},
+		{"microbit", "examples/serial", 2816, 388, 8, 2256},
 		{"wioterminal", "examples/pininterrupt", 6091, 1485, 116, 6816},
 
 		// TODO: also check wasm. Right now this is difficult, because
