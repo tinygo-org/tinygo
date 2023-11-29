c6609a02fab8e8839c3a71bd838f4773920fca33
deadprogram <ron@hybridgroup.com>
2023-11-29 09:56:34 +0100

    machine/esp32c3: move i2c implementation into separate file to skip m5stamp-c3 since it does not appear to expose those pins
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- src/machine/i2c.go
+++ src/machine/i2c.go
@@ -1,4 +1,4 @@
-//go:build atmega || nrf || sam || stm32 || fe310 || k210 || rp2040 || mimxrt1062 || esp32c3
+//go:build atmega || nrf || sam || stm32 || fe310 || k210 || rp2040 || mimxrt1062 || (esp32c3 && !m5stamp_c3)
 
 package machine
 
