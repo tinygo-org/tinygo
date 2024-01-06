02578a8f0f80898080164918d54497a344ec5527
Ayke van Laethem <aykevanlaethem@gmail.com>
2024-01-06 13:35:51 +0100

    all: switch to LLVM 17 by default

--- .github/workflows/sizediff-install-pkgs.sh
+++ .github/workflows/sizediff-install-pkgs.sh
@@ -2,11 +2,11 @@
 # still works after checking out the dev branch (that is, when going from LLVM
 # 16 to LLVM 17 for example, both Clang 16 and Clang 17 are installed).
 
-echo 'deb https://apt.llvm.org/jammy/ llvm-toolchain-jammy-16 main' | sudo tee /etc/apt/sources.list.d/llvm.list
+echo 'deb https://apt.llvm.org/jammy/ llvm-toolchain-jammy-17 main' | sudo tee /etc/apt/sources.list.d/llvm.list
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install --no-install-recommends -y \
-    llvm-16-dev \
-    clang-16 \
-    libclang-16-dev \
-    lld-16
+    llvm-17-dev \
+    clang-17 \
+    libclang-17-dev \
+    lld-17
