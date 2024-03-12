6d539586d56b7bc9a37ced13152b7983d1fa0c36
Scott Feldman <sfeldma@gmail.com>
2024-03-11 21:13:51 -0700

    add tinygo syscall support
    Stub out all the syscall entry points.  Basically replace
    src/syscall_{linux|unix}.go with src/syscall_tinygo.go.  It'll take some
    experimentation to discover which syscalls need to be implemented.  For
    networking, we can explore with the examples/net examples.
    
    The two files exec_tinygo.go and netlink_tinygo.go are copies of
    {exec|netlink}_linux.go files, and have stubbed out functions.  These
    files are copied only to work around an import cycle.  The original
    files import "sync", but that creates an import cycle:
    
    package tinygo.org/x/drivers/examples/net/tcpclient
           imports bytes
           imports io
           imports sync
           imports internal/task
           imports runtime/interrupt
           imports device/arm
           imports syscall
           imports sync: import cycle not allowed
    
    If we can solve the import cycle, we don't need these two new files.

---
+++ src/syscall/exec_tinygo.go
@@ -0,0 +1,41 @@
+package syscall
+
+// This is the original ForkLock:
+//
+// var ForkLock sync.RWMutex
+//
+// This requires importing sync, but importing sync causes an import loop:
+//
+// package tinygo.org/x/drivers/examples/net/tcpclient
+//        imports bytes
+//        imports io
+//        imports sync
+//        imports internal/task
+//        imports runtime/interrupt
+//        imports device/arm
+//        imports syscall
+//        imports sync: import cycle not allowed
+//
+// So for now, make our own ForkLock that doesn't use sync..
+
+type forklock struct {
+}
+
+func (f forklock) RLock() {
+	println("ForkLock.RLock not implemented")
+}
+
+func (f forklock) RUnlock() {
+	println("ForkLock.RUnlock not implemented")
+}
+
+var ForkLock forklock
+
+func CloseOnExec(fd int) {
+	println("CloseOnExec not implemented")
+}
+
+func SetNonblock(fd int, nonblocking bool) (err error) {
+	println("SetNonblock not implemented", fd, nonblocking)
+	return EOPNOTSUPP
+}
