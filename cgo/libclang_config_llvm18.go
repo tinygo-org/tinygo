bdb410b26f8bdf2b2a2950a2d791c3807df3d0e0
Ayke van Laethem <aykevanlaethem@gmail.com>
2024-05-18 12:55:35 +0200

    LLVM 18 support

---
+++ cgo/libclang_config_llvm18.go
@@ -0,0 +1,15 @@
+//go:build !byollvm && !llvm15 && !llvm16 && !llvm17
+
+package cgo
+
+/*
+#cgo linux        CFLAGS:  -I/usr/include/llvm-18 -I/usr/include/llvm-c-18 -I/usr/lib/llvm-18/include
+#cgo darwin,amd64 CFLAGS:  -I/usr/local/opt/llvm@18/include
+#cgo darwin,arm64 CFLAGS:  -I/opt/homebrew/opt/llvm@18/include
+#cgo freebsd      CFLAGS:  -I/usr/local/llvm18/include
+#cgo linux        LDFLAGS: -L/usr/lib/llvm-18/lib -lclang
+#cgo darwin,amd64 LDFLAGS: -L/usr/local/opt/llvm@18/lib -lclang
+#cgo darwin,arm64 LDFLAGS: -L/opt/homebrew/opt/llvm@18/lib -lclang
+#cgo freebsd      LDFLAGS: -L/usr/local/llvm18/lib -lclang
+*/
+import "C"
