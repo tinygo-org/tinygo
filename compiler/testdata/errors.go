461b7ca5e6edf95a84490a22e043473a8686a7f6
Randy Reddig <randy.reddig@fastly.com>
2024-06-29 05:30:48 -0400

    compiler: add additional test cases for wasm return types

--- compiler/testdata/errors.go
+++ compiler/testdata/errors.go
@@ -33,20 +33,37 @@ func validparam(a int32, b uint64, c float64, d unsafe.Pointer, e Uint, f uintpt
 //go:wasmimport modulename invalidparam
 func invalidparam(a [4]uint32, b []byte, c struct{ a int }, d chan struct{}, e func())
 
-//go:wasmimport modulename validreturn
-func validreturn() int32
+//go:wasmimport modulename validreturn1
+func validreturn1() int32
+
+//go:wasmimport modulename validreturn2
+func validreturn2() int
+
+//go:wasmimport modulename validreturn3
+func validreturn3() *int32
+
+//go:wasmimport modulename validreturn4
+func validreturn4() *S
+
+//go:wasmimport modulename validreturn5
+func validreturn5() unsafe.Pointer
 
 // ERROR: //go:wasmimport modulename manyreturns: too many return values
 //
 //go:wasmimport modulename manyreturns
 func manyreturns() (int32, int32)
 
-// ERROR: //go:wasmimport modulename invalidreturn: unsupported result type int
+// ERROR: //go:wasmimport modulename invalidreturn1: unsupported result type func()
 //
-//go:wasmimport modulename invalidreturn
-func invalidreturn() int
+//go:wasmimport modulename invalidreturn1
+func invalidreturn1() func()
 
-// ERROR: //go:wasmimport modulename invalidUnsafePointerReturn: unsupported result type unsafe.Pointer
+// ERROR: //go:wasmimport modulename invalidreturn2: unsupported result type []byte
 //
-//go:wasmimport modulename invalidUnsafePointerReturn
-func invalidUnsafePointerReturn() unsafe.Pointer
+//go:wasmimport modulename invalidreturn2
+func invalidreturn2() []byte
+
+// ERROR: //go:wasmimport modulename invalidreturn3: unsupported result type chan int
+//
+//go:wasmimport modulename invalidreturn3
+func invalidreturn3() chan int
