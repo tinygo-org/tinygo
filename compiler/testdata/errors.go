bcdb4194d50e666e425585f4275cacc25f26694f
Randy Reddig <randy.reddig@fastly.com>
2024-06-29 05:16:10 -0400

    compiler: add test cases for relaxed wasm types

--- compiler/testdata/errors.go
+++ compiler/testdata/errors.go
@@ -13,8 +13,25 @@ func implementation() {
 
 type Uint uint32
 
+type S struct {
+	a [4]uint32
+	b uintptr
+	c int
+	d float32
+	e float64
+}
+
 //go:wasmimport modulename validparam
-func validparam(a int32, b uint64, c float64, d unsafe.Pointer, e Uint)
+func validparam(a int32, b uint64, c float64, d unsafe.Pointer, e Uint, f uintptr, g *int32, h *S)
+
+// ERROR: //go:wasmimport modulename invalidparam: unsupported parameter type [4]uint32
+// ERROR: //go:wasmimport modulename invalidparam: unsupported parameter type []byte
+// ERROR: //go:wasmimport modulename invalidparam: unsupported parameter type struct{a int}
+// ERROR: //go:wasmimport modulename invalidparam: unsupported parameter type chan struct{}
+// ERROR: //go:wasmimport modulename invalidparam: unsupported parameter type func()
+//
+//go:wasmimport modulename invalidparam
+func invalidparam(a [4]uint32, b []byte, c struct{ a int }, d chan struct{}, e func())
 
 //go:wasmimport modulename validreturn
 func validreturn() int32
@@ -28,3 +45,8 @@ func manyreturns() (int32, int32)
 //
 //go:wasmimport modulename invalidreturn
 func invalidreturn() int
+
+// ERROR: //go:wasmimport modulename invalidUnsafePointerReturn: unsupported result type unsafe.Pointer
+//
+//go:wasmimport modulename invalidUnsafePointerReturn
+func invalidUnsafePointerReturn() unsafe.Pointer
