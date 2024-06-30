8da060f2dc38bf7d3956bf41793a4945992f07ea
Randy Reddig <randy.reddig@fastly.com>
2024-06-30 09:10:49 +0200

    compiler/testdata: improve test function names
    Address PR feedback.

--- compiler/testdata/errors.go
+++ compiler/testdata/errors.go
@@ -33,42 +33,45 @@ func validparam(a int32, b uint64, c float64, d unsafe.Pointer, e Uint, f uintpt
 //go:wasmimport modulename invalidparam
 func invalidparam(a [4]uint32, b []byte, c struct{ a int }, d chan struct{}, e func())
 
-//go:wasmimport modulename validreturn1
-func validreturn1() int32
+//go:wasmimport modulename validreturn_int32
+func validreturn_int32() int32
 
-//go:wasmimport modulename validreturn2
-func validreturn2() int
+//go:wasmimport modulename validreturn_int
+func validreturn_int() int
 
-//go:wasmimport modulename validreturn3
-func validreturn3() *int32
+//go:wasmimport modulename validreturn_ptr_int32
+func validreturn_ptr_int32() *int32
 
-//go:wasmimport modulename validreturn4
-func validreturn4() *S
+//go:wasmimport modulename validreturn_ptr_string
+func validreturn_ptr_string() *string
 
-//go:wasmimport modulename validreturn5
-func validreturn5() unsafe.Pointer
+//go:wasmimport modulename validreturn_ptr_struct
+func validreturn_ptr_struct() *S
+
+//go:wasmimport modulename validreturn_unsafe_pointer
+func validreturn_unsafe_pointer() unsafe.Pointer
 
 // ERROR: //go:wasmimport modulename manyreturns: too many return values
 //
 //go:wasmimport modulename manyreturns
 func manyreturns() (int32, int32)
 
-// ERROR: //go:wasmimport modulename invalidreturn1: unsupported result type func()
+// ERROR: //go:wasmimport modulename invalidreturn_func: unsupported result type func()
 //
-//go:wasmimport modulename invalidreturn1
-func invalidreturn1() func()
+//go:wasmimport modulename invalidreturn_func
+func invalidreturn_func() func()
 
-// ERROR: //go:wasmimport modulename invalidreturn2: unsupported result type []byte
+// ERROR: //go:wasmimport modulename invalidreturn_slice_byte: unsupported result type []byte
 //
-//go:wasmimport modulename invalidreturn2
-func invalidreturn2() []byte
+//go:wasmimport modulename invalidreturn_slice_byte
+func invalidreturn_slice_byte() []byte
 
-// ERROR: //go:wasmimport modulename invalidreturn3: unsupported result type chan int
+// ERROR: //go:wasmimport modulename invalidreturn_chan_int: unsupported result type chan int
 //
-//go:wasmimport modulename invalidreturn3
-func invalidreturn3() chan int
+//go:wasmimport modulename invalidreturn_chan_int
+func invalidreturn_chan_int() chan int
 
-// ERROR: //go:wasmimport modulename invalidreturn4: unsupported result type string
+// ERROR: //go:wasmimport modulename invalidreturn_string: unsupported result type string
 //
-//go:wasmimport modulename invalidreturn4
-func invalidreturn4() string
+//go:wasmimport modulename invalidreturn_string
+func invalidreturn_string() string
