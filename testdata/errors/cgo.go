3de78fcdc64450fafcd8fb1f15b0e51fb2c2ea41
Ayke van Laethem <aykevanlaethem@gmail.com>
2024-07-09 17:26:08 +0200

    libclang: do not make error locations relative
    This is done at a later time anyway, so doesn't need to be done in the
    cgo package. In fact, _not_ doing it there makes it easier to print
    correct relative packages.

--- testdata/errors/cgo.go
+++ testdata/errors/cgo.go
@@ -7,9 +7,6 @@ import "C"
 func main() {
 }
 
-// TODO: this error should be relative to the current directory (so cgo.go
-// instead of testdata/errors/cgo.go).
-
 // ERROR: # command-line-arguments
-// ERROR: testdata/errors/cgo.go:3:5: error: hello
-// ERROR: testdata/errors/cgo.go:4:4: error: expected identifier or '('
+// ERROR: cgo.go:3:5: error: hello
+// ERROR: cgo.go:4:4: error: expected identifier or '('
