4967002abc2ab2167f9bf500a71359568b698562
Ayke van Laethem <aykevanlaethem@gmail.com>
2024-07-09 17:16:32 +0200

    all: add testing for compiler error messages
    This is needed for some improvements I'm going to make next.
    
    This commit also refactors error handling slightly to make it more
    easily testable, this should hopefully not result in any actual changes
    in behavior.

---
+++ testdata/errors/types.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	var a int
+	a = "foobar"
+	nonexisting()
+}
+
+// ERROR: # command-line-arguments
+// ERROR: types.go:5:6: cannot use "foobar" (untyped string constant) as int value in assignment
+// ERROR: types.go:6:2: undefined: nonexisting
+// ERROR: types.go:4:6: a declared and not used
