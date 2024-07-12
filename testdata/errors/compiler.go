f4ce11ef37ea155ccf04e43590739a893b98a865
Ayke van Laethem <aykevanlaethem@gmail.com>
2024-07-12 17:05:04 +0200

    main: add error tests for the compiler
    The compiler can in fact return errors, but these weren't tested yet.

---
+++ testdata/errors/compiler.go
@@ -0,0 +1,13 @@
+package main
+
+//go:wasmimport foo bar
+func foo() {
+}
+
+//go:align 7
+var global int
+
+// TODO: include package name in the error message
+
+// ERROR: compiler.go:4:6: can only use //go:wasmimport on declarations
+// ERROR: compiler.go:8:5: global variable alignment must be a positive power of two
