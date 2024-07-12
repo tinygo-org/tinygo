e409950492340d0e74db681de25f4a2a0dc84b37
Ayke van Laethem <aykevanlaethem@gmail.com>
2024-07-12 14:30:15 +0200

    main: refactor error messages to use FileCheck-like patterns
    Match the error messages in testdata/errors/*.go like LLVM FileCheck,
    which includes regular expressions.
    
    I believe this is much more flexible, and LLVM uses it in nearly all of
    their tests so it must work quite well for compilers.

--- testdata/errors/loader-invaliddep.go
+++ testdata/errors/loader-invaliddep.go
@@ -5,4 +5,4 @@ import _ "github.com/tinygo-org/tinygo/testdata/errors/invaliddep"
 func main() {
 }
 
-// ERROR: invaliddep/invaliddep.go:1:1: expected 'package', found ppackage
+// ERROR: invaliddep{{[\\/]}}invaliddep.go:1:1: expected 'package', found ppackage
