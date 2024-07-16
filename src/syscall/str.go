02939ce6d012d9ce2b327eb96d22d0f419a1cb47
leongross <leon.gross@9elements.com>
2024-07-16 16:14:51 +0200

    add IndexByteString to syscall package to impede use of internal/bytealg
    Signed-off-by: leongross <leon.gross@9elements.com>

--- src/syscall/str.go
+++ src/syscall/str.go
@@ -4,6 +4,8 @@
 
 package syscall
 
+import "errors"
+
 // clen returns the index of the first NULL byte in n or len(n) if n contains no NULL byte.
 func clen(n []byte) int {
 	for i := 0; i < len(n); i++ {
@@ -12,4 +14,40 @@ func clen(n []byte) int {
 		}
 	}
 	return len(n)
+}
+
+// BytePtrFromString returns a pointer to a NUL-terminated array of
+// bytes containing the text of s. If s contains a NUL byte at any
+// location, it returns (nil, [EINVAL]).
+//
+// For darwin || nintendoswitch || wasi || wasip1 this is already implemented in /src/syscall/syscall_libc.go:266:6
+// func BytePtrFromString(s string) (*byte, error) {
+// 	a, err := ByteSliceFromString(s)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &a[0], nil
+// }
+
+// copied from upstream src/internal/bytealg/indexbyte_generic.go since we cannot use the internal bytealg package
+func IndexByteString(s string, c byte) int {
+	for i := 0; i < len(s); i++ {
+		if s[i] == c {
+			return i
+		}
+	}
+	return -1
+}
+
+// ByteSliceFromString returns a NUL-terminated slice of bytes
+// containing the text of s. If s contains a NUL byte at any
+// location, it returns (nil, [EINVAL]).
+// https://cs.opensource.google/go/go/+/master:src/syscall/syscall.go;l=45;drc=94982a07825aec711f11c97283e99e467838d616
+func ByteSliceFromString(s string) ([]byte, error) {
+	if IndexByteString(s, 0) != -1 {
+		return nil, errors.New("contains NUL")
+	}
+	a := make([]byte, len(s)+1)
+	copy(a, s)
+	return a, nil
 }
--- src/syscall/syscall.go
+++ src/syscall/syscall.go
@@ -2,19 +2,12 @@ package syscall
 
 import (
 	"errors"
-
-	"internal/bytealg"
 )
 
 const (
 	MSG_DONTWAIT = 0x40
 	AF_INET      = 0x2
 	AF_INET6     = 0xa
-)
-
-const (
-	// #define	EINVAL	22	/* Invalid argument			*/
-	EINVAL = 22
 )
 
 func Exit(code int)
@@ -26,28 +19,4 @@ type Rlimit struct {
 
 func Setrlimit(resource int, rlim *Rlimit) error {
 	return errors.New("Setrlimit not implemented")
-}
-
-// ByteSliceFromString returns a NUL-terminated slice of bytes
-// containing the text of s. If s contains a NUL byte at any
-// location, it returns (nil, [EINVAL]).
-// https://cs.opensource.google/go/go/+/master:src/syscall/syscall.go;l=45;drc=94982a07825aec711f11c97283e99e467838d616
-func ByteSliceFromString(s string) ([]byte, error) {
-	if bytealg.IndexByteString(s, 0) != -1 {
-		return nil, errors.New("contains NUL")
-	}
-	a := make([]byte, len(s)+1)
-	copy(a, s)
-	return a, nil
-}
-
-// BytePtrFromString returns a pointer to a NUL-terminated array of
-// bytes containing the text of s. If s contains a NUL byte at any
-// location, it returns (nil, [EINVAL]).
-func BytePtrFromString(s string) (*byte, error) {
-	a, err := ByteSliceFromString(s)
-	if err != nil {
-		return nil, err
-	}
-	return &a[0], nil
 }
