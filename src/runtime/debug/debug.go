f90b5c10fad1b47f663aa4eacdf2dfc81c1dda14
Laurent Demailly <ldemailly@gmail.com>
2024-07-18 10:27:46 -0700

    review comment: added (c) header inspired from runtime/timer.go

--- src/runtime/debug/debug.go
+++ src/runtime/debug/debug.go
@@ -1,4 +1,8 @@
-// Package debug is a dummy package that is not yet implemented.
+// Portions copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package debug is a very partially implemented package to allow compilation.
 package debug
 
 import (
