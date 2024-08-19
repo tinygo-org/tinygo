16cd500000174dce35a8ab6da2e7b98fc3d839cd
Ayke van Laethem <aykevanlaethem@gmail.com>
2024-08-19 19:56:43 +0200

    ci: update apt repo for sizediff toolchain
    This was still at jammy (22.04), while the CI container was noble
    (24.04). Somehow this didn't break, but it certainly isn't ideal to
    install packages across Ubuntu versions!

--- .github/workflows/sizediff-install-pkgs.sh
+++ .github/workflows/sizediff-install-pkgs.sh
@@ -2,7 +2,7 @@
 # still works after checking out the dev branch (that is, when going from LLVM
 # 16 to LLVM 17 for example, both Clang 16 and Clang 17 are installed).
 
-echo 'deb https://apt.llvm.org/jammy/ llvm-toolchain-jammy-18 main' | sudo tee /etc/apt/sources.list.d/llvm.list
+echo 'deb https://apt.llvm.org/noble/ llvm-toolchain-noble-18 main' | sudo tee /etc/apt/sources.list.d/llvm.list
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install --no-install-recommends -y \
