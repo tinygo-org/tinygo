d5ae0163f6fc6824eaf818a12589ae933d1253f3
leongross <leon.gross@9elements.com>
2024-09-11 14:22:33 +0200

    remove print statements for cleaner execution
    Signed-off-by: leongross <leon.gross@9elements.com>

--- src/runtime/netpoll.go
+++ src/runtime/netpoll.go
@@ -23,23 +23,23 @@ const (
 
 //go:linkname poll_runtime_pollReset internal/poll.runtime_pollReset
 func poll_runtime_pollReset(pd *pollDesc, mode int) int {
-	println("poll_runtime_pollReset not implemented", pd, mode)
+	// println("poll_runtime_pollReset not implemented", pd, mode)
 	return pollNoError
 }
 
 //go:linkname poll_runtime_pollWait internal/poll.runtime_pollWait
 func poll_runtime_pollWait(pd *pollDesc, mode int) int {
-	println("poll_runtime_pollWait not implemented", pd, mode)
+	// println("poll_runtime_pollWait not implemented", pd, mode)
 	return pollNoError
 }
 
 //go:linkname poll_runtime_pollSetDeadline internal/poll.runtime_pollSetDeadline
 func poll_runtime_pollSetDeadline(pd *pollDesc, d int64, mode int) {
-	println("poll_runtime_pollSetDeadline not implemented", pd, d, mode)
+	// println("poll_runtime_pollSetDeadline not implemented", pd, d, mode)
 }
 
 //go:linkname poll_runtime_pollOpen internal/poll.runtime_pollOpen
 func poll_runtime_pollOpen(fd uintptr) (*pollDesc, int) {
-	println("poll_runtime_pollOpen not implemented", fd)
+	// println("poll_runtime_pollOpen not implemented", fd)
 	return &pollDesc{runtimeCtx: 0x13371337}, pollNoError
 }
