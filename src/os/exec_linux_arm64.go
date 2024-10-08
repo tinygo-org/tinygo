4b015ac141958ee23be62511ea8fa813b3632ac9
leongross <leon.gross@9elements.com>
2024-10-08 14:02:34 +0200

    add signal implementations for linux arm64

--- src/os/exec_linux_arm64.go
+++ src/os/exec_linux_arm64.go
@@ -6,7 +6,29 @@
 
 package os
 
-import "errors"
+import (
+	"errors"
+	"runtime"
+	"syscall"
+)
+
+var (
+	Interrupt Signal = syscall.SIGINT
+	Kill      Signal = syscall.SIGKILL
+)
+
+// Keep compatible with golang and always succeed and return new proc with pid on Linux.
+func findProcess(pid int) (*Process, error) {
+	return &Process{Pid: pid}, nil
+}
+
+func (p *Process) release() error {
+	// NOOP for unix.
+	p.Pid = -1
+	// no need for a finalizer anymore
+	runtime.SetFinalizer(p, nil)
+	return nil
+}
 
 // On the aarch64 architecture, the fork system call is not available.
 // Therefore, the fork function is implemented to return an error.
