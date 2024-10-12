2690b243ea0a47a52fd2fbaea95cdbe4e6f01bb8
Ayke van Laethem <aykevanlaethem@gmail.com>
2024-10-12 10:50:34 +0200

    main: make sure typecheck errors are correctly reported

--- errors_test.go
+++ errors_test.go
@@ -25,6 +25,7 @@ func TestErrors(t *testing.T) {
 		{name: "compiler"},
 		{name: "interp"},
 		{name: "invalidmain"},
+		{name: "invalidname"},
 		{name: "linker-flashoverflow", target: "cortex-m-qemu"},
 		{name: "linker-ramoverflow", target: "cortex-m-qemu"},
 		{name: "linker-undefined", target: "darwin/arm64"},
