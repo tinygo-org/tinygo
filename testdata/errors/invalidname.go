415307e4c1196df835c61b4c2384cd8b76d8f40d
Ayke van Laethem <aykevanlaethem@gmail.com>
2024-10-12 10:50:34 +0200

    main: make sure typecheck errors are correctly reported

---
+++ testdata/errors/invalidname.go
@@ -0,0 +1,6 @@
+package main
+
+import _ "github.com/tinygo-org/tinygo/testdata/errors/invalidname"
+
+// ERROR: # github.com/tinygo-org/tinygo/testdata/errors/invalidname
+// ERROR: invalidname{{[\\/]}}invalidname.go:3:9: invalid package name _
