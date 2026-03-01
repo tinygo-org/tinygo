694912d9d2cbd8697061cf0f59d337b3b7bd0122
Dima Jolkin <dimajolkin@gmail.com>
2026-03-01 18:32:25 +0200

    esp32s3-adc: change adc tests

--- src/examples/adc/esp32xx.go
+++
@@ -1,7 +0,0 @@
-//go:build esp32s3 || esp32c3
-
-package main
-
-import "machine"
-
-const led = machine.NoPin
--- src/examples/adc/pca10040.go
+++
@@ -1,7 +0,0 @@
-//go:build pca10040
-
-package main
-
-import "machine"
-
-const led = machine.LED
