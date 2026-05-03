8a7f0855f78aac41b98e6479ddbc92e2ed950d7c
Matthew Hiles <matthew.hiles@gmail.com>
2026-05-03 12:58:28 -0400

    add rest of STOP methods (direct UEFI ABI only)

--- src/machine/uefi/simple_text_out.go
+++ src/machine/uefi/simple_text_out.go
@@ -24,6 +24,44 @@ type EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL struct {
 	Mode              *EFI_SIMPLE_TEXT_OUTPUT_MODE
 }
 
+func (p *EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL) Reset(extendedVerification BOOLEAN) EFI_STATUS {
+	return UefiCall2(p.reset, uintptr(unsafe.Pointer(p)), booleanArg(extendedVerification))
+}
+
 func (p *EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL) OutputString(s *CHAR16) EFI_STATUS {
 	return UefiCall2(p.outputString, uintptr(unsafe.Pointer(p)), uintptr(unsafe.Pointer(s)))
+}
+
+func (p *EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL) TestString(s *CHAR16) EFI_STATUS {
+	return UefiCall2(p.testString, uintptr(unsafe.Pointer(p)), uintptr(unsafe.Pointer(s)))
+}
+
+func (p *EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL) QueryMode(modeNumber UINTN, columns *UINTN, rows *UINTN) EFI_STATUS {
+	return UefiCall4(
+		p.queryMode,
+		uintptr(unsafe.Pointer(p)),
+		uintptr(modeNumber),
+		uintptr(unsafe.Pointer(columns)),
+		uintptr(unsafe.Pointer(rows)),
+	)
+}
+
+func (p *EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL) SetMode(modeNumber UINTN) EFI_STATUS {
+	return UefiCall2(p.setMode, uintptr(unsafe.Pointer(p)), uintptr(modeNumber))
+}
+
+func (p *EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL) SetAttribute(attribute UINTN) EFI_STATUS {
+	return UefiCall2(p.setAttribute, uintptr(unsafe.Pointer(p)), uintptr(attribute))
+}
+
+func (p *EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL) ClearScreen() EFI_STATUS {
+	return UefiCall1(p.clearScreen, uintptr(unsafe.Pointer(p)))
+}
+
+func (p *EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL) SetCursorPosition(column UINTN, row UINTN) EFI_STATUS {
+	return UefiCall3(p.setCursorPosition, uintptr(unsafe.Pointer(p)), uintptr(column), uintptr(row))
+}
+
+func (p *EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL) EnableCursor(visible BOOLEAN) EFI_STATUS {
+	return UefiCall2(p.enableCursor, uintptr(unsafe.Pointer(p)), booleanArg(visible))
 }
