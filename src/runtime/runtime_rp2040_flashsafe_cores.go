f8ff2e485a3e4c7a51cfc9b2a1d61624d237e4fb
rdon <you@example.com>
2026-05-20 17:57:17 +0900

    gofmt files

--- src/runtime/runtime_rp2040_flashsafe_cores.go
+++ src/runtime/runtime_rp2040_flashsafe_cores.go
@@ -3,19 +3,19 @@
 package runtime
 
 import (
-    "device/arm"
-    "device/rp"
-    "runtime/interrupt"
-    "runtime/volatile"
-    _ "unsafe" // required for //go:section
+	"device/arm"
+	"device/rp"
+	"runtime/interrupt"
+	"runtime/volatile"
+	_ "unsafe" // required for //go:section
 )
 
 const (
-    rp2040FlashSafeFIFOCommand = 2
+	rp2040FlashSafeFIFOCommand = 2
 
-    rp2040FlashSafeIdle    = 0
-    rp2040FlashSafeLocked  = 1
-    rp2040FlashSafeRelease = 2
+	rp2040FlashSafeIdle    = 0
+	rp2040FlashSafeLocked  = 1
+	rp2040FlashSafeRelease = 2
 )
 
 // rp2040FlashSafeState is used to synchronize the core that performs a flash
@@ -37,50 +37,50 @@ var flashSafeLock = spinLock{id: 24}
 // handler and waits until it acknowledges that it is parked. Local interrupts
 // are disabled after the other core is parked.
 func rp2040EnterFlashSafeSection() interrupt.State {
-    if !secondaryCoresStarted {
-        return interrupt.Disable()
-    }
+	if !secondaryCoresStarted {
+		return interrupt.Disable()
+	}
 
-    flashSafeLock.Lock()
+	flashSafeLock.Lock()
 
-    core := currentCPU()
-    rp2040FlashSafeState.Set(rp2040FlashSafeIdle)
+	core := currentCPU()
+	rp2040FlashSafeState.Set(rp2040FlashSafeIdle)
 
-    for i := uint32(0); i < numCPU; i++ {
-        if i == core {
-            continue
-        }
-        rp2040FlashSafePauseCore(i)
-    }
+	for i := uint32(0); i < numCPU; i++ {
+		if i == core {
+			continue
+		}
+		rp2040FlashSafePauseCore(i)
+	}
 
-    for rp2040FlashSafeState.Get() != rp2040FlashSafeLocked {
-        spinLoopWait()
-    }
+	for rp2040FlashSafeState.Get() != rp2040FlashSafeLocked {
+		spinLoopWait()
+	}
 
-    return interrupt.Disable()
+	return interrupt.Disable()
 }
 
 // rp2040ExitFlashSafeSection exits a section entered by
 // rp2040EnterFlashSafeSection.
 func rp2040ExitFlashSafeSection(state interrupt.State) {
-    if secondaryCoresStarted {
-        rp2040FlashSafeState.Set(rp2040FlashSafeRelease)
-        arm.Asm("sev")
+	if secondaryCoresStarted {
+		rp2040FlashSafeState.Set(rp2040FlashSafeRelease)
+		arm.Asm("sev")
 
-        for rp2040FlashSafeState.Get() != rp2040FlashSafeIdle {
-            spinLoopWait()
-        }
+		for rp2040FlashSafeState.Get() != rp2040FlashSafeIdle {
+			spinLoopWait()
+		}
 
-        flashSafeLock.Unlock()
-    }
+		flashSafeLock.Unlock()
+	}
 
-    interrupt.Restore(state)
+	interrupt.Restore(state)
 }
 
 func rp2040FlashSafePauseCore(core uint32) {
-    _ = core // RP2040 SIO FIFO writes to the other core.
-    rp.SIO.FIFO_WR.Set(rp2040FlashSafeFIFOCommand)
-    arm.Asm("sev")
+	_ = core // RP2040 SIO FIFO writes to the other core.
+	rp.SIO.FIFO_WR.Set(rp2040FlashSafeFIFOCommand)
+	arm.Asm("sev")
 }
 
 // rp2FlashSafeInterruptHandler runs on the other core while this core is
@@ -93,19 +93,19 @@ func rp2040FlashSafePauseCore(core uint32) {
 //
 //go:section .ramfuncs
 func rp2FlashSafeInterruptHandler(core uint32) {
-    _ = core
+	_ = core
 
-    state := interrupt.Disable()
+	state := interrupt.Disable()
 
-    rp2040FlashSafeState.Set(rp2040FlashSafeLocked)
-    arm.Asm("sev")
+	rp2040FlashSafeState.Set(rp2040FlashSafeLocked)
+	arm.Asm("sev")
 
-    for rp2040FlashSafeState.Get() == rp2040FlashSafeLocked {
-        arm.Asm("wfe")
-    }
+	for rp2040FlashSafeState.Get() == rp2040FlashSafeLocked {
+		arm.Asm("wfe")
+	}
 
-    interrupt.Restore(state)
+	interrupt.Restore(state)
 
-    rp2040FlashSafeState.Set(rp2040FlashSafeIdle)
-    arm.Asm("sev")
+	rp2040FlashSafeState.Set(rp2040FlashSafeIdle)
+	arm.Asm("sev")
 }
