aef70a5839012a6109ad0d3b9ad4aababfaa6558
sago35 <sago35@gmail.com>
2026-06-07 20:32:52 +0900

    machine/usb: fix truncated USB string descriptor when host requests short maxLen (#5449)
    * machine/usb: fix truncated USB string descriptor when host requests short maxLen
    * fix binary size

--- builder/sizes_test.go
+++ builder/sizes_test.go
@@ -44,7 +44,7 @@ func TestBinarySize(t *testing.T) {
 		// microcontrollers
 		{"hifive1b", "examples/echo", 3817, 299, 0, 2252},
 		{"microbit", "examples/serial", 2820, 356, 8, 2248},
-		{"wioterminal", "examples/pininterrupt", 7286, 1534, 120, 7248},
+		{"wioterminal", "examples/pininterrupt", 7330, 1550, 120, 7248},
 
 		// TODO: also check wasm. Right now this is difficult, because
 		// wasm binaries are run through wasm-opt and therefore the
