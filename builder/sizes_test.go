11c2b76e2974b1806d04d5ab3fdd265c6e5c064c
Nia Waldvogel <niaow1234@gmail.com>
2026-06-09 12:35:12 -0400

    runtime (gc.blocks): move objHeader to the end
    This moves the objHeader from before an object body to after it.
    On 32-bit systems with 16-byte alignment requirements (x86, ARM, RISC-V), we previously padded the header to a whole block.
    This wastes up to 12 bytes, as on -gc=conservative the header is a single pointer.
    With this change, no padding is required (beyond that from rounding the size up).
    
    The "head" block in the metadata was moved to the end of the range to match the header location.
    This changed the block loop directions throughout the GC logic.
    The bit hacks used by sweep no longer work because there is no equivalent of addition that carries downwards.
    However it is now possible to merge the sweep and free range list rebuild passes because their loop directions match.
    
    There are two other places where we rebuilt the free ranges list: when initializing or growing the heap.
    The former can be easily replaced with a single hardcoded range containing the entire heap.
    In the latter case, I opted to only add the new space to the existing list.
    These replacements allowed me to fully remove the buildFreeRanges function.

--- builder/sizes_test.go
+++ builder/sizes_test.go
@@ -42,9 +42,9 @@ func TestBinarySize(t *testing.T) {
 	// This is a small number of very diverse targets that we want to test.
 	tests := []sizeTest{
 		// microcontrollers
-		{"hifive1b", "examples/echo", 3817, 299, 0, 2252},
-		{"microbit", "examples/serial", 2820, 356, 8, 2248},
-		{"wioterminal", "examples/pininterrupt", 8020, 1652, 132, 7480},
+		{"hifive1b", "examples/echo", 3705, 299, 0, 2252},
+		{"microbit", "examples/serial", 2736, 356, 8, 2248},
+		{"wioterminal", "examples/pininterrupt", 7960, 1652, 132, 7480},
 
 		// TODO: also check wasm. Right now this is difficult, because
 		// wasm binaries are run through wasm-opt and therefore the
