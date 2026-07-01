ff7e5ec17f9cd76843b2d7c970d6a95df139f9b5
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-07-01 17:37:42 -0300

    feat: bump minimum node version to 22
    Drops official support for NodeJS 18 for WASM by removing the provided polyfills in the `wasm_exec.js`.
    
    Now the environment is expected to provide the polyfill if it is running on older NodeJS versions
    
    The new minimum required version for WASM is NodeJS 22+.

--- testdata/wasmfunc.js
+++ testdata/wasmfunc.js
@@ -1,12 +1,4 @@
-const crypto = require('crypto');
 const fs = require('fs')
-
-// Polyfill required to maintain compatibility with NodeJS 18
-globalThis.crypto = {
-    getRandomValues(b) {
-        crypto.randomFillSync(b);
-    },
-};
 
 require('../targets/wasm_exec.js');
 
