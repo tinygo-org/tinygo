cf5bed822721f54f802b637739801f0d189271b4
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2026-07-15 09:05:27 -0500

    sync: fix deadlock in Map.Range callback
    Currently, calling any sync.Map method from inside the sync.Map.Range
    callback f deadlocks. Moreover, Go's sync.Map explicitly permits the
    Range callback to call other methods on the map ("Range does not
    block other methods on the receiver; even f itself may call any
    method on m").
    
    This commit prevents the deadlock by changing sync.Map.Range to:
    - copy the map's keys under the lock
    - release the lock
    - iterate over the key snapshot
    
    A snapshot satisfies Go's sync.Map.Range contract, which only
    requires that no key is visited more than once and may reflect any
    mapping from any point during the call.
    
    Using a snapshot keeps the implementation simple, in line with this
    file's stated scope ("no more efficient than a map with a lock").
    
    Also added TestMapRangeAndDelete regression test, which deletes map
    entries from inside the map's Range callback.

--- src/sync/map_test.go
+++ src/sync/map_test.go
@@ -36,3 +36,31 @@ func TestMapSwap(t *testing.T) {
 		t.Errorf("Load after Swap returned %v, %v, want foo, true", v, ok)
 	}
 }
+
+func TestMapRangeAndDelete(t *testing.T) {
+	var sm sync.Map
+	sm.Store(0, "0")
+	sm.Store(1, "1")
+	sm.Store(2, "2")
+
+	sm.Range(func(k, v any) bool {
+		keyAsInt, ok := k.(int)
+		if !ok {
+			return true
+		}
+		if keyAsInt%2 == 0 {
+			sm.Delete(keyAsInt)
+		}
+		return true
+	})
+
+	if v, ok := sm.Load(0); ok {
+		t.Errorf("Load(0) after Delete returned %v, %v, want nil, false", v, ok)
+	}
+	if v, ok := sm.Load(1); !ok || v.(string) != "1" {
+		t.Errorf("Load(1) after Delete returned %v, %v, want \"1\", true", v, ok)
+	}
+	if v, ok := sm.Load(2); ok {
+		t.Errorf("Load(2) after Delete returned %v, %v, want nil, false", v, ok)
+	}
+}
