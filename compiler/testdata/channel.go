bd88768d3cd47ed5b84d833a3944e3884b5eaaf5
rdon-key <rdonster@gmail.com>
2026-07-19 01:21:31 +0900

    compiler: optimize non-blocking single-case selects

--- compiler/testdata/channel.go
+++ compiler/testdata/channel.go
@@ -23,3 +23,48 @@ func selectZeroRecv(ch1 chan int, ch2 chan struct{}) {
 	default:
 	}
 }
+
+func selectNonBlockingSend(ch chan int, value int) bool {
+	select {
+	case ch <- value:
+		return true
+	default:
+		return false
+	}
+}
+
+func selectNonBlockingRecv(ch chan int) (int, bool, bool) {
+	select {
+	case value, ok := <-ch:
+		return value, ok, true
+	default:
+		return 0, false, false
+	}
+}
+
+func selectNonBlockingZeroSend(ch chan struct{}) bool {
+	select {
+	case ch <- struct{}{}:
+		return true
+	default:
+		return false
+	}
+}
+
+func selectNonBlockingZeroRecv(ch chan struct{}) (bool, bool) {
+	select {
+	case _, ok := <-ch:
+		return ok, true
+	default:
+		return false, false
+	}
+}
+
+func selectBlocking(ch1, ch2 chan int) (int, bool) {
+	select {
+	case value, ok := <-ch1:
+		return value, ok
+	case value, ok := <-ch2:
+		return value, ok
+	}
+}
