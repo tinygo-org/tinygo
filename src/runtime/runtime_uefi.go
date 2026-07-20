4a11f0b16aab94f967b83b7ee2e0b030741c4779
Matthew Hiles <matthew.hiles@gmail.com>
2026-07-20 12:45:22 -0400

    swap lines around

--- src/runtime/runtime_uefi.go
+++ src/runtime/runtime_uefi.go
@@ -55,12 +55,12 @@ func sleepTicks(d timeUnit) {
 }
 
 func putchar(c byte) {
-	buf := [2]uefi.CHAR16{uefi.CHAR16(c), 0}
 	if c == '\n' {
 		buf := [4]uefi.CHAR16{'\r', 0, '\n', 0}
 		uefi.ST().ConOut.OutputString(&buf[0])
 		return
 	}
+	buf := [2]uefi.CHAR16{uefi.CHAR16(c), 0}
 	uefi.ST().ConOut.OutputString(&buf[0])
 }
 
