fb02824ae09f0bd50d630dac2f32cecf526bb76c
yohimik <yohimik@gmail.com>
2026-09-05 06:28:14 +0400

    builder: declare Darwin socket and process symbols

--- builder/darwin-libsystem.go
+++ builder/darwin-libsystem.go
@@ -1,12 +1,48 @@
 package builder
 
 import (
+	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/tinygo-org/tinygo/compileopts"
 	"github.com/tinygo-org/tinygo/goenv"
 )
+
+// Extra declarations for libSystem exports absent from macos-minimal-sdk.
+// See https://github.com/tinygo-org/macos-minimal-sdk for the SDK sources.
+var darwinExtraLibSystemSymbols = []string{
+	// The BSD socket API, which the host netdev in src/net reaches through
+	// the standard library syscall package.
+	"accept",
+	"bind",
+	"connect",
+	"getpeername",
+	"getsockname",
+	"getsockopt",
+	"listen",
+	"recvfrom",
+	"recvmsg",
+	"sendmsg",
+	"sendto",
+	"setsockopt",
+	"shutdown",
+	"socket",
+	"socketpair",
+
+	// addchdir_np requires macOS 10.15 or later.
+	"posix_spawn",
+	"posix_spawn_file_actions_addchdir_np",
+	"posix_spawn_file_actions_addclose",
+	"posix_spawn_file_actions_adddup2",
+	"posix_spawn_file_actions_destroy",
+	"posix_spawn_file_actions_init",
+	"posix_spawnattr_destroy",
+	"posix_spawnattr_init",
+	"posix_spawnattr_setflags",
+	"posix_spawnattr_setpgroup",
+	"posix_spawnattr_setsigmask",
+}
 
 // Create a job that builds a Darwin libSystem.dylib stub library. This library
 // contains all the symbols needed so that we can link against it, but it
@@ -36,7 +72,34 @@ func makeDarwinLibSystemJob(config *compileopts.Config, tmpdir string) *compileJ
 				return err
 			}
 
-			// Link object file to dynamic library.
+			// Compile the extra stubs into a second object file, so that the
+			// generated one stays as it is.
+			extrapath := filepath.Join(tmpdir, "libSystem-extra.s")
+			extraobjpath := filepath.Join(tmpdir, "libSystem-extra.o")
+			var extra strings.Builder
+			extra.WriteString("// Stubs for symbols exported by libSystem but not declared in lib/macos-minimal-sdk.\n")
+			for _, symbol := range darwinExtraLibSystemSymbols {
+				extra.WriteString("\n.global _" + symbol + "\n_" + symbol + ":\n")
+			}
+			if err := os.WriteFile(extrapath, []byte(extra.String()), 0o666); err != nil {
+				return err
+			}
+			flags = []string{
+				"-nostdlib",
+				"--target=" + config.Triple(),
+				"-c",
+				"-o", extraobjpath,
+				extrapath,
+			}
+			if config.Options.PrintCommands != nil {
+				config.Options.PrintCommands("clang", flags...)
+			}
+			err = runCCompiler(flags...)
+			if err != nil {
+				return err
+			}
+
+			// Link object files to dynamic library.
 			platformVersion := strings.TrimPrefix(strings.Split(config.Triple(), "-")[2], "macosx")
 			flags = []string{
 				"-flavor", "darwin",
@@ -48,6 +111,7 @@ func makeDarwinLibSystemJob(config *compileopts.Config, tmpdir string) *compileJ
 				"-install_name", "/usr/lib/libSystem.B.dylib",
 				"-o", job.result,
 				objpath,
+				extraobjpath,
 			}
 			if config.Options.PrintCommands != nil {
 				config.Options.PrintCommands("ld.lld", flags...)
