93940cb6eaf5c0e018534072453a796ef99416c7
Navendu Pottekkat <navendu@apache.org>
2026-09-17 23:47:15 +0530

    esp32: route GPIO pin interrupts to the PRO CPU (#5688)
    * esp32: route GPIO pin interrupts to the PRO CPU
    
    Signed-off-by: Navendu Pottekkat <navendu@apache.org>
    
    * fix: add explainer comment
    
    Signed-off-by: Navendu Pottekkat <navendu@apache.org>
    
    ---------
    
    Signed-off-by: Navendu Pottekkat <navendu@apache.org>

--- src/machine/machine_esp32.go
+++ src/machine/machine_esp32.go
@@ -407,9 +407,11 @@ func (p Pin) SetInterrupt(change PinChange, callback func(Pin)) error {
 		return setupPinInterruptErr
 	}
 
+	// Bit 2 is the PRO CPU interrupt enable. See ESP32 TRM section 6.13.1,
+	// register 6.30 GPIO_PINn_REG.
 	p.pinReg().Set(
 		(p.pinReg().Get() & ^uint32(esp.GPIO_PIN_INT_TYPE_Msk|esp.GPIO_PIN_INT_ENA_Msk)) |
-			uint32(change)<<esp.GPIO_PIN_INT_TYPE_Pos | uint32(1)<<esp.GPIO_PIN_INT_ENA_Pos)
+			uint32(change)<<esp.GPIO_PIN_INT_TYPE_Pos | uint32(4)<<esp.GPIO_PIN_INT_ENA_Pos)
 
 	return nil
 }
